1ab7b626f3a2a712bc6e5de815d2dc0269ada135
Gustavo Rosa <gth.rosa@uol.com.br>
2017-11-01 17:38:08 -0200

    Improving agents.py and adding first utils module (random).

--- opytimizer/core/agent.py
+++ opytimizer/core/agent.py
@@ -13,4 +13,11 @@ class Agent(object):
         self.n = n
         self.x = np.zeros(n)
         self.fit = 0
-    
+
+    def CheckLimits(self, LB, UB):
+        # Check an Agent limits based on lower and upper bounds
+        for i in range(self.n):
+            if self.x[i] < LB[i]:
+                self.x[i] = LB[i]
+            elif self.x[i] > UB[i]:
+                self.x[i] = UB[i]
