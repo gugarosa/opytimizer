c77591d1aecd43857aa1c845c3d12c0fc2720959
Gustavo Rosa <gth.rosa@uol.com.br>
2017-11-08 14:38:04 -0200

    PyLinting random module.

--- opytimizer/utils/random.py
+++ opytimizer/utils/random.py
@@ -1,16 +1,35 @@
-import numpy as np
-
-"""
-A random numbers generate module.
-Differents distributions are used in order to generate numbers.
+"""A random numbers generate module.
+   Differents distributions are used in order to generate numbers.
 """
 
-def GenerateUniformRandomNumber(low, high, size=None):
-    # Generates a random number based on an uniform distribution
-    r = np.random.uniform(low, high, size)
-    return r
+import numpy.random as np
 
-def GenerateGaussianRandomNumber(mean, variance, size=None):
-    # Generates a random number based on a gaussian distribution
-    r = np.random.normal(mean, variance, size)
-    return r
+
+def generate_uniform_random_number(low, high, size=None):
+    """Generates a random number based on an uniform distribution.
+
+        # Arguments
+            low: lower interval
+            high: higher interval
+            size: size of array
+
+        # Returns
+            uniform_array: uniform random array
+    """
+    uniform_array = np.random.uniform(low, high, size)
+    return uniform_array
+
+
+def generate_gaussian_random_number(mean, variance, size=None):
+    """Generates a random number based on a gaussian distribution.
+
+        # Arguments
+            mean: gaussian's mean value
+            variance: gaussian's variance value
+            size: size of array
+
+        # Returns
+            gaussian_array: gaussian random array
+    """
+    gaussian_array = np.random.normal(mean, variance, size)
+    return gaussian_array
