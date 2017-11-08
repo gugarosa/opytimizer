3c73956580bddaf645e842d45a0b6bef6d99d57a
Gustavo Rosa <gth.rosa@uol.com.br>
2017-11-08 13:16:05 -0200

    Working on Agent's object.

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,3 +1,16 @@
 import opytimizer.core.agent as Agent
+import numpy as np
 
-a = Agent.Agent(10)
+# Instanciate a new agent
+a = Agent.Agent(n_variables=2, n_dimensions=1)
+
+# Test check_limits method
+LB = np.zeros(a.n_variables)
+UB = np.zeros(a.n_variables)
+
+for i in range(a.n_variables):
+    LB[i] = 1
+    UB[i] = 3
+    
+a.check_limits(LB, UB)
+print(a.x)
