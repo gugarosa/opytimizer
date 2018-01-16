2880dc8d671288974eed458047c51c5b01917dd6
Gustavo Rosa <gth.rosa@uol.com.br>
2018-01-16 15:21:00 -0200

    Adding check functions to math module.

--- opytimizer/utils/math.py
+++ opytimizer/utils/math.py
@@ -3,8 +3,9 @@
 
     # Methods
     norm(vector): Calculates the norm over a vector.
-    span(vector, lower_bound, upper_bound): Spans a value between lower and upper bound.
-    check_limit()
+    span(vector, lower_bound, upper_bound): Spans the vector to a value between lower and upper bound.
+    check_unitary(vector): Check if vector is between 0 and 1.
+    check_bounds(vector, lower_bound, upper_bound): Check if vector is between lower and upper bounds.
 """
 
 import numpy as np
@@ -20,14 +21,17 @@ def norm(vector=None):
         total: Total value of norm function over the chosen variable.
     """
     somatory = 0.0
+    # Iterate through all vector
     for i in range(vector.size):
+        # Adds to somatory its squared element
         somatory += (vector[i] ** 2)
+    # Apply square root to all somatory variable
     total = np.sqrt(somatory)
     return total
 
 
 def span(vector=None, lower_bound=None, upper_bound=None):
-    """ Spans a value between lower and upper bound.
+    """ Spans the vector to a value between lower and upper bound.
 
         # Arguments
         vector: vector to span value.
@@ -37,17 +41,39 @@ def span(vector=None, lower_bound=None, upper_bound=None):
         # Returns
         value: Spanned value between lower and upper bounds.
     """
+    # Calculate the vector's norm
     vector_norm = norm(vector)
+    # Span its value between lower and upper bounds
     value = (upper_bound - lower_bound) * \
         (vector_norm / np.sqrt(vector.size)) + lower_bound
     return value
 
 
-def check_limit(vector=None, lower_bound=None, upper_bound=None):
+def check_unitary(vector=None):
+    """ Check if vector is between 0 and 1.
+
+        # Arguments
+        vector: vector containing the position values.
+
+        # Returns
+        vector: vector with values between 0 and 1.
+    """
+    # Iterate through all vector dimensions
+    for i in range(vector.size):
+        # If value is lower than 0, update it to 0
+        if vector[i] < 0:
+            vector[i] = 0
+            # If value is greater than 1, update it to 1
+        elif vector[i] > 1:
+            vector[i] = 1
+    return vector
+
+
+def check_bounds(vector=None, lower_bound=None, upper_bound=None):
     """ Check if vector is between lower and upper bounds.
 
         # Arguments
-        vector: vector containing the posicion values.
+        vector: vector containing the position values.
         lower_bound: lower bound value.
         upper_bound: upper bound value.
 
