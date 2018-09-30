87afd1e614c0bc35c2bf898e799911f66b047ee6
Gustavo Rosa <gth.rosa@uol.com.br>
2018-09-30 14:52:33 -0300

    Adding new code relared to Space.

--- examples/initialize_agent.py
+++ examples/initialize_agent.py
@@ -1,7 +1,7 @@
 from opytimizer.core.agent import Agent
 
 # Creates a new Agent
-a = Agent()
+a = Agent(n_variables=3, n_dimensions=2)
 
 # Prints important information about this agent
 print(a.position)
---
+++ examples/initialize_space.py
@@ -0,0 +1,11 @@
+from opytimizer.core.space import Space
+
+# Creates a new Space
+s = Space(n_agents=2)
+
+# Prior to use the Space, you need to build it
+s.build(n_variables=5, n_dimensions=1)
+
+# You can print out the whole Space or acess
+# individual agents and its properties
+print(s.agents[0].position)
