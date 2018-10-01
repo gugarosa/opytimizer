aa79ebb8b97dd00af2d6876f5b07f92d776f1c8d
Gustavo Rosa <gth.rosa@uol.com.br>
2018-10-01 11:20:50 -0300

    Seems to be more appropriate now.

--- examples/initialize_space.py
+++ examples/initialize_space.py
@@ -1,11 +1,22 @@
 from opytimizer.core.space import Space
 
+# Input parameters
+n_agents = 2
+n_variables = 5
+n_dimensions = 1
+n_iterations = 5
+
+# Bounds parameters
+lower_bound = [0.1, 0.3, 0.5, 0.5, 0.5]
+upper_bound = [0.2, 0.4, 2.0, 2.0, 2.0]
+
 # Creates a new Space
-s = Space(n_agents=2)
+s = Space(n_agents=n_agents, n_variables=n_variables,
+          n_dimensions=n_dimensions, n_iterations=n_iterations)
 
 # Prior to use the Space, you need to build so,
 # its agents are disponible for further use
-s.build(n_variables=5, n_dimensions=1)
+s.build(lower_bound=lower_bound, upper_bound=upper_bound)
 
 # You can print out the whole Space or acess
 # individual agents and its properties
--- opytimizer/core/space.py
+++ opytimizer/core/space.py
@@ -1,5 +1,6 @@
 import numpy as np
 import opytimizer.utils.logging as l
+import opytimizer.utils.random as r
 
 from opytimizer.core.agent import Agent
 
@@ -10,34 +11,102 @@ class Space:
     """
     """
 
-    def __init__(self, n_agents=1):
+    def __init__(self, n_agents=1, n_variables=2, n_dimensions=1, n_iterations=10):
         """
         """
 
         logger.info('Initializing Space ...')
 
-        # External variables that can be accessed
+        # Space useful variables
         self.n_agents = n_agents
+        self.n_variables = n_variables
+        self.n_dimensions = n_dimensions
+        self.n_iterations = n_iterations
+
+        # Space agents variables
         self.agents = []
         self.best_agent = None
 
-        # Internal use only
+        # Space bounds variables
+        self.lb = np.zeros(n_variables)
+        self.ub = np.ones(n_variables)
+
+        # Internal use variables
         self._built = False
+
+        # Creating space's agents
+        self._create_agents(n_variables, n_dimensions)
 
         # We will log some important information
         logger.info('Space created.')
         logger.info('Number of agents: ' + str(n_agents))
+        logger.info('Number of iterations: ' + str(n_iterations))
 
-    def build(self, n_variables=2, n_dimensions=1):
+    def _create_agents(self, n_variables, n_dimensions):
+        """
+        """
+
+        logger.debug('Running private method: _create_agents()')
+
+        for i in range(self.n_agents):
+            self.agents.append(
+                Agent(n_variables=n_variables, n_dimensions=n_dimensions))
+
+    def _initialize_agents(self):
+        """
+        """
+
+        logger.debug('Running private method: _initialize_agents()')
+
+        for i in range(self.n_agents):
+            for j in range(self.n_variables):
+                self.agents[i].position[j] = r.generate_uniform_random_number(
+                        self.lb[j], self.ub[j], size=self.n_dimensions)
+
+        logger.debug('Agents were initialized.')
+
+    def _check_bound_size(self, size, bound):
+        """
+        """
+
+        logger.debug('Running private method: _check_bound_size()')
+
+        if len(bound) != size:
+            e = 'Bound needs to be the same size of number of variables.'
+            logger.error(e)
+            raise RuntimeError(e)
+        else:
+            logger.debug('Bounds were checked without errors.')
+            return True
+
+    def build(self, lower_bound=None, upper_bound=None):
         """
         """
 
         logger.debug('Running method: build()')
 
-        for i in range(self.n_agents):
-            self.agents.append(Agent(n_variables=n_variables, n_dimensions=n_dimensions))
+        # Checking if lower bound is avaliable
+        if lower_bound:
+            # We also need to check if its size matches to our
+            # actual number of variables
+            if self._check_bound_size(self.n_variables, lower_bound):
+                self.lb = lower_bound
 
+        # Checking if upper bound is avaliable
+        if upper_bound:
+            # We also need to check if its size matches to our
+            # actual number of variables
+            if self._check_bound_size(self.n_variables, upper_bound):
+                self.ub = upper_bound
+
+        # As now we can assume the bounds are correct, we need to
+        # initialize our agents
+        self._initialize_agents()
+
+        # If no errors were shown, we can declared the Space as built
         self._built = True
+
+        logger.debug('Space was successfully built.')
 
     def call(self):
         """
@@ -49,4 +118,3 @@ class Space:
             raise RuntimeError(e)
 
         logger.debug('Running method: call()')
-
