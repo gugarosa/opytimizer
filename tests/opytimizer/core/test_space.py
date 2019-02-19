2813f0ddbd285c90630e6c1c9711062311856eac
Gustavo Rosa <gth.rosa@uol.com.br>
2019-02-19 10:46:10 -0300

    Few more tests.

--- tests/opytimizer/core/test_space.py
+++ tests/opytimizer/core/test_space.py
@@ -1,8 +1,39 @@
 import pytest
 
-from opytimizer.core import function
-from opytimizer.utils import math
+from opytimizer.core import space
 
-f = function.Function(expression='x0', lower_bound=1, upper_bound=2)
-f.call()
-f.evaluate()
+
+def test_space_n_agents():
+    lb = [-1, -1]
+    ub = [1, 1]
+
+    s = space.Space(lower_bound=lb, upper_bound=ub)
+
+    assert s.n_agents == 1
+
+
+def test_space_n_variables():
+    lb = [-1, -1]
+    ub = [1, 1]
+
+    s = space.Space(lower_bound=lb, upper_bound=ub)
+
+    assert s.n_variables == 2
+
+
+def test_space_n_dimensions():
+    lb = [-1, -1]
+    ub = [1, 1]
+
+    s = space.Space(lower_bound=lb, upper_bound=ub)
+
+    assert s.n_dimensions == 1
+
+
+def test_space_n_iterations():
+    lb = [-1, -1]
+    ub = [1, 1]
+
+    s = space.Space(lower_bound=lb, upper_bound=ub)
+
+    assert s.n_iterations == 10
