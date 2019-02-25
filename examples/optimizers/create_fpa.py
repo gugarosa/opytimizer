c64fdd72869f22b5573b6acac0eef4bfce66f04b
DouglasRodrigues <douglasrodrigues.dr@gmail.com>
2019-02-25 17:03:23 -0300

    Adding Flower Pollination Algorithm

---
+++ examples/optimizers/create_fpa.py
@@ -0,0 +1,12 @@
+from opytimizer.optimizers.fpa import FPA
+
+# One should declare a hyperparameters object based
+# on the desired algorithm that will be used
+hyperparams = {
+    'beta': 1.5,
+    'eta': 0.2,
+    'p': 0.8
+}
+
+# Creating a FPA optimizer
+p = FPA(hyperparams=hyperparams)
