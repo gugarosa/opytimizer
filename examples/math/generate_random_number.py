efb6ff3b9b1059598345e47d5658bd627f4eaea8
Gustavo Rosa <gth.rosa@uol.com.br>
2019-02-26 13:34:58 -0300

    Adding math package examples.

---
+++ examples/math/generate_random_number.py
@@ -0,0 +1,9 @@
+import opytimizer.math.random as r
+
+# Generating a random uniform number array
+u = r.generate_uniform_random_number(low=0.0, high=1.0, size=10)
+print(u)
+
+# Generating a random gaussian number array
+g = r.generate_gaussian_random_number(mean=0.5, variance=1.0, size=10)
+print(g)
