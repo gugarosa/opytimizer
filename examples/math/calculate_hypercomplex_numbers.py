765228b5c85283e845d5c6756df4c3c6c31217e6
Gustavo Rosa <gth.rosa@uol.com.br>
2019-02-27 17:53:44 -0300

    Fixing up some typos.

--- examples/math/calculate_hypercomplex_numbers.py
+++ examples/math/calculate_hypercomplex_numbers.py
@@ -4,6 +4,7 @@ import opytimizer.math.hypercomplex as h
 
 # Creating an array with ones
 a = np.ones((2, 4))
+print(f'Array: {a}')
 
 # Declaring lower bounds
 lb = np.array([-5, -5])
@@ -13,12 +14,8 @@ ub = np.array([-2, -2])
 
 # Calculating the hypercomplex number norm
 norm = h.norm(a)
+print(f'Norm Array: {norm}')
 
 # Spanning it into lower and upper bounds
 span = h.span(a, lb, ub)
-
-# Printing outputs
-print(f'Array: {a}')
-print(f'Norm Array: {norm}')
 print(f'Spanned Array: {span}')
-
