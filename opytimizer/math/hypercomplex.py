971ac461da9e7461f34590004471e2dc6c5c0133
Gustavo Rosa <gth.rosa@uol.com.br>
2019-02-27 15:17:58 -0300

    Forgot to add docstrings in math.hypercomplex module.

--- opytimizer/math/hypercomplex.py
+++ opytimizer/math/hypercomplex.py
@@ -1,7 +1,17 @@
 import numpy as np
 
+
 def norm(array):
-    """
+    """Calculates the norm over an array. It is used as the first step to map
+    a hypercomplex number to a real-valued space.
+
+    Args:
+        array (np.array): A 2-dimensional input array.
+
+    Returns:
+        The norm calculated over the second axis. For example: (2, 4) array shape
+        will result in a norm (2, ) shape.
+
     """
 
     # Calculating the norm over the hypercomplex numbers
@@ -9,8 +19,19 @@ def norm(array):
 
     return array_norm
 
+
 def span(array, lb, ub):
-    """
+    """Spans a hypercomplex number between lower and upper bounds.
+
+    Args:
+        array (np.array): A 2-dimensional input array.
+        lb (list | np.array): Lower bounds to be spanned.
+        ub (list | np.array) Upper bounds to be spanned.
+
+    Returns:
+        A spanned value that can be used as decision variable in order to
+        feed a fitness function.
+
     """
 
     # We need to force lower bound to be an array
@@ -23,4 +44,3 @@ def span(array, lb, ub):
     array_span = (ub - lb) * (norm(array) / np.sqrt(array.shape[1])) + lb
 
     return array_span
-
