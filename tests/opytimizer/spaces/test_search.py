bf74e0a797aa140389cf6a8797f3c0af90a0cfbc
Gustavo Rosa <gth.rosa@uol.com.br>
2019-03-07 13:12:45 -0300

    Still missing a few tests prior to next release.

---
+++ tests/opytimizer/spaces/test_search.py
@@ -0,0 +1,26 @@
+import pytest
+from opytimizer.spaces import search
+
+
+def test_search_initialize_agents():
+    lb = [0, 0]
+
+    ub = [10, 10]
+
+    new_search_space = search.SearchSpace(lower_bound=lb, upper_bound=ub)
+
+    assert new_search_space.agents[0].position[0] != 0
+
+
+def test_search_check_bound_limits():
+    lb = [0, 0]
+
+    ub = [10, 10]
+
+    new_search_space = search.SearchSpace(lower_bound=lb, upper_bound=ub)
+
+    new_search_space.agents[0].position[0] = 20
+
+    new_search_space.check_bound_limits(new_search_space.agents, lb, ub)
+
+    assert new_search_space.agents[0].position[0] != 20
