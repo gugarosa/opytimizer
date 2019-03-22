94ecf522aa0d056fcd733019973148db565a894b
Gustavo Rosa <gth.rosa@uol.com.br>
2019-03-22 13:25:03 -0300

    Correcting few typos recently found.

--- docs/conf.py
+++ docs/conf.py
@@ -92,8 +92,6 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -147,7 +145,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'opytimizer.tex', 'opytimizer Documentation',
+    (master_doc, 'opytimizer.tex', 'Opytimizer Documentation',
      'Gustavo de Rosa', 'manual'),
 ]
 
@@ -157,7 +155,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'opytimizer', 'opytimizer Documentation',
+    (master_doc, 'opytimizer', 'Opytimizer Documentation',
      [author], 1)
 ]
 
@@ -168,7 +166,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'opytimizer', 'opytimizer Documentation',
+    (master_doc, 'opytimizer', 'Opytimizer Documentation',
      author, 'opytimizer', 'One line description of project.',
      'Miscellaneous'),
 ]
