13b9fdc83e482b8956c2e389112da5c3a0644494
Gustavo Rosa <gth.rosa@uol.com.br>
2019-03-22 10:42:58 -0300

    This might fix building problem with readthedocs.

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import sys
 import opytimizer
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
