591431a800b922a00c33a2460c950c4253bc25ce
Gustavo Rosa <gth.rosa@uol.com.br>
2019-04-16 18:06:34 -0300

    Updating wrong parameters for PSO's example.

--- examples/optimizers/create_pso.py
+++ examples/optimizers/create_pso.py
@@ -3,9 +3,9 @@ from opytimizer.optimizers.pso import PSO
 # One should declare a hyperparameters object based
 # on the desired algorithm that will be used
 hyperparams = {
-    'w': 2.5,
-    'c1': 1.5,
-    'c2': 2
+    'w': 0.7,
+    'c1': 1.7,
+    'c2': 1.7
 }
 
 # Creating a PSO optimizer
