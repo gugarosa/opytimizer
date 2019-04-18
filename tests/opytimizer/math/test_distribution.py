1368deb4da6b7f7fa88c7f3b957efea18c7eaa5d
Gustavo Rosa <gth.rosa@uol.com.br>
2019-04-18 14:11:40 -0300

    Reworking tests. Missing functions and optimizers.

--- tests/opytimizer/math/test_distribution.py
+++ tests/opytimizer/math/test_distribution.py
@@ -1,10 +1,13 @@
 import pytest
 from opytimizer.math import distribution
 
+
 def test_generate_bernoulli_distribution():
-    bernoulli_array = distribution.generate_bernoulli_distribution(prob=0.5, size=10)
+    bernoulli_array = distribution.generate_bernoulli_distribution(
+        prob=0.5, size=10)
 
     assert bernoulli_array.shape == (10, )
+
 
 def test_generate_levy_distribution():
     levy_array = distribution.generate_levy_distribution(beta=0.1, size=5)
