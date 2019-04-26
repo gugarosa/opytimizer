2c965cf611fc19b177e0b950267df269304f35b6
Gustavo Rosa <gth.rosa@uol.com.br>
2019-04-26 14:08:42 -0300

    Working on History class rework. Tests will break for several commits.

--- opytimizer/utils/history.py
+++ opytimizer/utils/history.py
@@ -5,7 +5,7 @@ class History:
     """A History class is responsible for saving each iteration's output.
 
     One can configure, if necessary, different properties or outputs that
-    can be saved. Currently, we only save each agent's position and its fitness.
+    can be saved. Currently, we only save agents' and best agent's position and fitness.
 
     """
 
@@ -14,27 +14,42 @@ class History:
 
         """
 
-        # Our history property will be first set as an empty list
-        self._history = []
+        # Our agents property will be first set as an empty list
+        self._agents = []
+
+        # Also, an empty list for the best agent property
+        self._best_agent = []
 
     @property
-    def history(self):
-        """list: A history property to hold agent's position and fitness.
+    def agents(self):
+        """list: An agents property to hold agents' position and fitness.
 
         """
 
-        return self._history
+        return self._agents
 
-    @history.setter
-    def history(self, history):
-        self._history = history
+    @agents.setter
+    def agents(self, agents):
+        self._agents = agents
 
-    
-    def dump(self, agents):
-        """Dumps a list of agents into the object.
+    @property
+    def best_agent(self):
+        """list: A best agent property to hold best agent's position and fitness.
+
+        """
+
+        return self._best_agent
+
+    @best_agent.setter
+    def best_agent(self, best_agent):
+        self._best_agent = best_agent
+
+    def dump(self, agents, best_agent):
+        """Dumps agents and best agent into the object
 
         Args:
             agents (list): List of agents.
+            best_agent (Agent): An instance of the best agent.
 
         """
 
@@ -45,22 +60,29 @@ class History:
         for agent in agents:
             # We append its position as a list and its fitness
             a.append((agent.position.tolist(), agent.fit))
-            
-        # Finally, we can append the current iteration list to our property
-        self.history.append(a)
+
+        # Finally, we can append the current iteration agents to our property
+        self.agents.append(a)
+
+        # Appending the best agent as well
+        self.best_agent.append((best_agent.position.tolist(), best_agent.fit))
 
     def show(self):
-        """Prints in a formatted way the history of agent's position
-        and fitness.
+        """Prints in a formatted way the history of agents' and best agent's 
+        position and fitness.
         
         """
 
         # For every iteration
-        for i, iteration in enumerate(self.history):
+        for i, (agents, best) in enumerate(zip(self.agents, self.best_agent)):
             print(f'\nIteration: {i+1}\n')
-            # And for every agent
-            for j, agent in enumerate(iteration):
+
+            # Iterating through every agent
+            for j, agent in enumerate(agents):
                 print(f'Agent[{j}]: {agent[0]} | Fitness: {agent[1]}')
+
+            print(f'Best agent: {best[0]} | Fitness: {best[1]}')
+
 
     def save(self, file_name):
         """Saves the object to a pickle encoding.
@@ -74,7 +96,7 @@ class History:
         f = open(file_name, 'wb')
 
         # Dumps to a pickle file
-        pickle.dump(self.history, f)
+        pickle.dump(self, f)
 
         # Close the file
         f.close()
@@ -87,7 +109,7 @@ class History:
             file_name (str): String containing pickle's file path.
 
         Returns:
-            The content of a History object loaded from a pickle file.
+            A History object loaded from a pickle file.
 
         """
 
@@ -95,4 +117,6 @@ class History:
         f = open(file_name, "rb")
 
         # Loads using pickle
-        self.history = pickle.load(f)
+        self = pickle.load(f)
+
+        return self
