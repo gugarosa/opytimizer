be085defaafddd034fc45a8b79ad2b67465b45ff
Gustavo Rosa <gth.rosa@uol.com.br>
2019-09-24 12:08:03 -0300

    Using own Node class. Guess it will be easier.

---
+++ opytimizer/utils/node.py
@@ -0,0 +1,19 @@
+class Node():
+    """
+    """
+
+    def __init__(self, id, name, type, right=None, left=None, parent=None):
+        """
+        """
+
+        self.id = id
+
+        self.name = name
+
+        self.type = type
+
+        self.right = right
+
+        self.left = left
+
+        self.parent = parent
