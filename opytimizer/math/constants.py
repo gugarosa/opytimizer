04ca4c374c8eb20a4ad51353eba7a0f2eee16117
Gustavo Rosa <gth.rosa@uol.com.br>
2019-09-25 11:15:02 -0300

    Changing TEST_EPSILON constant to 5.

--- opytimizer/math/constants.py
+++ opytimizer/math/constants.py
@@ -7,4 +7,4 @@ EPSILON = 10e-10
 # (which is always the sphere function, i.e. sum(x^2) and has global minimum at 0)
 # is smaller than this value. We use a rather large margin to avoid tests failing
 # in CI caused by bad initialization points by the agents
-TEST_EPSILON = 2
+TEST_EPSILON = 5
