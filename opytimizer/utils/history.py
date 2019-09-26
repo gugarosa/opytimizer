c78c980e210f2a5fde74d3948f1ae3c50584ec7c
Gustavo Rosa <gth.rosa@uol.com.br>
2019-09-26 01:00:05 -0300

    Starting to rework the History class.

--- opytimizer/utils/history.py
+++ opytimizer/utils/history.py
@@ -44,6 +44,24 @@ class History:
     def best_agent(self, best_agent):
         self._best_agent = best_agent
 
+    def __str__(self):
+        """Prints in a formatted way the history of agents' and best agent's 
+        position, fitness and index.
+
+        """
+
+        # For every iteration
+        for i, (agents, best) in enumerate(zip(self.agents, self.best_agent)):
+            print(f'\nIteration: {i+1}\n')
+
+            # Iterating through every agent
+            for j, agent in enumerate(agents):
+                print(f'Agent[{j}]: {agent[0]} | Fitness: {agent[1]}')
+
+            print(f'\nBest = Agent[{best[2]}]: {best[0]} | Fitness: {best[1]}')
+            
+        return ''
+
     def dump(self, agents, best_agent, best_index=0):
         """Dumps agents and best agent into the object.
 
@@ -88,46 +106,32 @@ class History:
         self.best_agent.append(
             (best_agent.position.tolist(), best_agent.fit, best_index))
 
-    def show(self):
-        """Prints in a formatted way the history of agents' and best agent's 
-        position, fitness and index.
-
-        """
-
-        # For every iteration
-        for i, (agents, best) in enumerate(zip(self.agents, self.best_agent)):
-            print(f'\nIteration: {i+1}\n')
-
-            # Iterating through every agent
-            for j, agent in enumerate(agents):
-                print(f'Agent[{j}]: {agent[0]} | Fitness: {agent[1]}')
-
-            print(f'Best = Agent[{best[2]}]: {best[0]} | Fitness: {best[1]}')
+    
 
     def save(self, file_name):
         """Saves the object to a pickle encoding.
 
         Args:
-            file_name (str): String holding the file's name that will be saved.
+            file_name (str): File's name to be saved.
 
         """
 
         # Opening a destination file
         with open(file_name, 'wb') as dest_file:
-            # Dumping History object to file
+            # Dumping History to file
             pickle.dump(self, dest_file)
 
     def load(self, file_name):
         """Loads the object from a pickle encoding.
 
         Args:
-            file_name (str): String containing pickle's file path.
+            file_name (str): Pickle's file path to be loaded.
 
         """
 
-        # Resetting current object state to loaded state
+        # Trying to open the file
         with open(file_name, "rb") as origin_file:
-            # Loading History object from file
+            # Loading History from file
             h = pickle.load(origin_file)
 
             # Updating all values
