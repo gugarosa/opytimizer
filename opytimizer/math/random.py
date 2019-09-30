add8ce0c40aadf622ba5f35f78a3c3a63b074dd5
Gustavo Rosa <gth.rosa@uol.com.br>
2019-09-30 11:52:37 -0300

    Fixing up reproduction operator.

--- opytimizer/math/random.py
+++ opytimizer/math/random.py
@@ -37,3 +37,29 @@ def generate_gaussian_random_number(mean=0.0, variance=1.0, size=1):
     gaussian_array = np.random.normal(mean, variance, size)
 
     return gaussian_array
+
+
+def tournament_selection(fitness, k):
+    """Selects `k` individuals based on a tournament selection algorithm.
+
+    Args:
+        fitness (list): List of individuals fitness.
+        k (int): Number of individuals to be selected.
+
+    Returns:
+        A list with the indexes of the selected individuals.
+
+    """
+
+    # Creating a list to append selected individuals
+    selected = []
+
+    # For every possible selection
+    for i in range(k):
+        # Calculate the probabilities of selecting an individual
+        possible = np.random.choice(fitness, k)
+
+        # Appends the selected index
+        selected.append(np.where(min(possible) == fitness)[0][0])
+
+    return selected
