e90bde5a22b660fc63edddd52f4b82ae628e6610
Gustavo Rosa <gth.rosa@uol.com.br>
2019-10-03 11:33:01 -0300

    Raising test epsilon constant to prevent travis from breaking.

--- opytimizer/utils/constants.py
+++ opytimizer/utils/constants.py
@@ -29,7 +29,7 @@ N_ARGS_FUNCTION = {
 
 # A test passes if the best solution found by the agent in the target function
 # is smaller than this value
-TEST_EPSILON = 5
+TEST_EPSILON = 10
 
 # When using the Tournament Selection, one must provide the size of rounds,
 # where individuals will compete among themselves
