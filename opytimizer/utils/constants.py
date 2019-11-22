77fe14bec2d71ae6e930f4e8822bf01eff8ac819
Gustavo Rosa <gth.rosa@uol.com.br>
2019-11-22 12:39:32 -0300

    Adding speed of light constant.

--- opytimizer/utils/constants.py
+++ opytimizer/utils/constants.py
@@ -12,6 +12,10 @@ FLOAT_MAX = sys.float_info.max
 # when using the History object
 HISTORY_KEYS = ['agents', 'best_agent', 'local']
 
+# When working with relativity theories, it is necessary
+# to defined a constant for the speed of light
+LIGHT_SPEED = 3e5
+
 # When using Genetic Programming, each function node needs an unique number of arguments,
 # which is defined by this dictionary
 N_ARGS_FUNCTION = {
