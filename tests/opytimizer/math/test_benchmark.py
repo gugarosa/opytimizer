52abf474264af829c2f0c6e8b9abe9eb7e7ab7a9
Gustavo Rosa <gth.rosa@uol.com.br>
2019-11-26 14:02:14 -0300

    Changing how to calculate benchmarking tests.

--- tests/opytimizer/math/test_benchmark.py
+++ tests/opytimizer/math/test_benchmark.py
@@ -9,7 +9,7 @@ def test_ackley1():
 
     y = benchmark.ackley1(x)
 
-    assert y != 0
+    assert np.round(y, 2) == 4.64
 
 
 def test_alpine1():
@@ -17,7 +17,7 @@ def test_alpine1():
 
     y = benchmark.alpine1(x)
 
-    assert y != 0
+    assert np.round(y, 2) == 2.46
 
 
 def test_alpine2():
@@ -25,7 +25,7 @@ def test_alpine2():
 
     y = benchmark.alpine2(x)
 
-    assert y != 0
+    assert np.round(y, 2) == -0.08
 
 
 def test_brown():
@@ -33,7 +33,7 @@ def test_brown():
 
     y = benchmark.brown(x)
 
-    assert y != 0
+    assert np.round(y, 2) == 3.42
 
 
 def test_chung_reynolds():
@@ -41,7 +41,7 @@ def test_chung_reynolds():
 
     y = benchmark.chung_reynolds(x)
 
-    assert y != 0
+    assert np.round(y, 2) == 6.25
 
 
 def test_cosine_mixture():
@@ -49,7 +49,7 @@ def test_cosine_mixture():
 
     y = benchmark.cosine_mixture(x)
 
-    assert y != 0
+    assert np.round(y, 2) == -2.7
 
 
 def test_csendes():
@@ -57,7 +57,7 @@ def test_csendes():
 
     y = benchmark.csendes(x)
 
-    assert y != 0
+    assert np.round(y, 2) == 5.77
 
 
 def test_deb1():
@@ -65,7 +65,7 @@ def test_deb1():
 
     y = benchmark.deb1(x)
 
-    assert y != 0
+    assert np.round(y, 2) == -0.5
 
 
 def test_deb2():
@@ -73,7 +73,7 @@ def test_deb2():
 
     y = benchmark.deb2(x)
 
-    assert y != 0
+    assert np.round(y, 2) == -0.16
 
 
 def test_exponential():
@@ -81,7 +81,15 @@ def test_exponential():
 
     y = benchmark.exponential(x)
 
-    assert y != 0
+    assert np.round(y, 2) == -0.29
+
+
+def test_quintic():
+    x = np.array([0.5, 0.5, 1, 1])
+
+    y = benchmark.quintic(x)
+
+    assert np.round(y, 2) == 36.31
 
 
 def test_rastringin():
@@ -89,7 +97,7 @@ def test_rastringin():
 
     y = benchmark.rastringin(x)
 
-    assert y != 0
+    assert np.round(y, 2) == 42.5
 
 
 def test_salomon():
@@ -97,7 +105,7 @@ def test_salomon():
 
     y = benchmark.salomon(x)
 
-    assert y != 0
+    assert np.round(y, 2) == 2.03
 
 
 def test_schwefel():
@@ -105,7 +113,15 @@ def test_schwefel():
 
     y = benchmark.schwefel(x)
 
-    assert y != 0
+    assert np.round(y, 2) == 1673.6
+
+
+def test_schumer_steiglitz():
+    x = np.array([0.5, 0.5, 1, 1])
+
+    y = benchmark.schumer_steiglitz(x)
+
+    assert np.round(y, 2) == 2.12
 
 
 def test_sphere():
@@ -113,4 +129,12 @@ def test_sphere():
 
     y = benchmark.sphere(x)
 
-    assert y != 0
+    assert np.round(y, 2) == 2.5
+
+
+def test_styblinski_tang():
+    x = np.array([0.5, 0.5, 1, 1])
+
+    y = benchmark.styblinski_tang(x)
+
+    assert np.round(y, 2) == -11.44
