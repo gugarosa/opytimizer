cd94f850d680d27bfa3f050b5046aafa4a85dca3
Gustavo Rosa <gth.rosa@uol.com.br>
2020-05-05 12:28:41 -0300

    Adding decorator unitary test.

---
+++ tests/opytimizer/utils/test_decorator.py
@@ -0,0 +1,15 @@
+import pytest
+
+from opytimizer.utils import decorator
+
+def test_pre_evaluation():
+    @decorator.pre_evaluation
+    def call(obj, x):
+        return x
+
+    def f():
+        return True
+
+    assert f() == True
+
+    call(f, 1, hook=None)
