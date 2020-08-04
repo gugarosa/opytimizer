dbb584a1fb28d6929407f0697f416d91f5dd0ddc
Gustavo Rosa <gth.rosa@uol.com.br>
2020-08-04 17:24:32 -0300

    Fixing up HS test.

--- tests/opytimizer/optimizers/evolutionary/test_hs.py
+++ tests/opytimizer/optimizers/evolutionary/test_hs.py
@@ -79,7 +79,7 @@ def test_hs_generate_new_harmony():
                                       n_variables=2, lower_bound=[0, 0],
                                       upper_bound=[10, 10])
 
-    agent = new_hs._generate_new_harmony(search_space.agents[0])
+    agent = new_hs._generate_new_harmony(search_space.agents)
 
     assert agent.fit > 0
 
