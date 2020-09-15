7a0f5dc439367ee4b56edb9f1147fc0bdd4e68ce
Gustavo Rosa <gth.rosa@uol.com.br>
2020-09-15 14:30:34 -0300

    Updating version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='opytimizer',
-      version='2.1.0',
+      version='2.1.1',
       description='Nature-Inspired Python Optimizer',
       long_description=long_description,
       long_description_content_type='text/markdown',
