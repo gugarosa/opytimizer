bbd9aaa19eea6c59d56790c2c43a94aa0d2f2dc4
Gustavo Rosa <gth.rosa@uol.com.br>
2021-04-28 10:36:30 -0300

    Fixing up history-based tests.

--- tests/opytimizer/utils/test_history.py
+++ tests/opytimizer/utils/test_history.py
@@ -1,30 +1,29 @@
-import os
-
 from opytimizer.core import agent
 from opytimizer.utils import history
 
 
-def test_history_store_best_only():
+def test_history_save_agents():
     new_history = history.History()
 
-    assert new_history.store_best_only is False
+    assert new_history.save_agents is False
 
 
-def test_history_store_best_only_setter():
+def test_history_save_agents_setter():
     new_history = history.History()
 
     try:
-        new_history.store_best_only = 'a'
+        new_history.save_agents = 'a'
     except:
-        new_history.store_best_only = True
+        new_history.save_agents = True
 
-    assert new_history.store_best_only is True
+    assert new_history.save_agents is True
 
 
 def test_history_dump():
-    new_history = history.History()
+    new_history = history.History(save_agents=True)
 
-    agents = [agent.Agent(n_variables=2, n_dimensions=1) for _ in range(5)]
+    agents = [agent.Agent(n_variables=2, n_dimensions=1, lower_bound=[
+                          0, 0], upper_bound=[1, 1]) for _ in range(5)]
 
     new_history.dump(agents=agents, best_agent=agents[4], value=0)
 
@@ -32,44 +31,48 @@ def test_history_dump():
     assert len(new_history.best_agent) > 0
     assert new_history.value[0] == 0
 
+    new_history = history.History(save_agents=False)
 
-def test_history_get():
-    new_history = history.History()
+    new_history.dump(agents=agents)
 
-    agents = [agent.Agent(n_variables=2, n_dimensions=1) for _ in range(5)]
+    assert hasattr(new_history, 'agents') is False
 
-    new_history.dump(agents=agents, best_agent=agents[4], value=0)
+
+def test_history_get_convergence():
+    new_history = history.History(save_agents=True)
+
+    agents = [agent.Agent(n_variables=2, n_dimensions=1, lower_bound=[
+                          0, 0], upper_bound=[1, 1]) for _ in range(5)]
+
+    new_history.dump(
+        agents=agents, best_agent=agents[4], local_position=agents[0].position, value=0)
+    new_history.dump(
+        agents=agents, best_agent=agents[4], local_position=agents[0].position, value=0)
 
     try:
-        agents = new_history.get(key='agents', index=0)
+        agents_pos, agents_fit = new_history.get_convergence(
+            key='agents', index=5)
     except:
-        agents = new_history.get(key='agents', index=(0, 0))
+        agents_pos, agents_fit = new_history.get_convergence(
+            key='agents', index=0)
+
+    assert agents_pos.shape == (2, 2)
+    assert agents_fit.shape == (2,)
+
+    best_agent_pos, best_agent_fit = new_history.get_convergence(
+        key='best_agent')
+
+    assert best_agent_pos.shape == (2, 2)
+    assert best_agent_fit.shape == (2,)
 
     try:
-        agents = new_history.get(key='agents', index=(0, 0, 0))
+        local_position = new_history.get_convergence(
+            key='local_position', index=5)
     except:
-        agents = new_history.get(key='agents', index=(0, 0))
+        local_position = new_history.get_convergence(key='local_position')
 
-    assert agents.shape == (2, 1)
+    assert local_position.shape == (2,)
 
+    value = new_history.get_convergence(key='value')
 
-def test_history_save():
-    new_history = history.History()
-
-    agents = [agent.Agent(n_variables=2, n_dimensions=1) for _ in range(5)]
-
-    new_history.dump(agents=agents, best_agent=agents[0])
-
-    new_history.save('models/test.pkl')
-
-    assert os.path.isfile('./models/test.pkl')
-
-
-def test_history_load():
-    new_history = history.History()
-
-    new_history.load('models/test.pkl')
-
-    assert len(new_history.agents) > 0
-
-    print(new_history)
+    assert value.shape == (2,)
