e25e04353463e22252b1728137fc9d3134fa5352
Gustavo Rosa <gth.rosa@uol.com.br>
2021-05-11 16:17:50 -0300

    Starting to implement swarm-based optimizers.

--- tests/opytimizer/optimizers/swarm/test_abc.py
+++ tests/opytimizer/optimizers/swarm/test_abc.py
@@ -1,11 +1,7 @@
 import numpy as np
 
-from opytimizer.core import function
 from opytimizer.optimizers.swarm import abc
 from opytimizer.spaces import search
-from opytimizer.utils import constant
-
-np.random.seed(0)
 
 
 def test_abc_params():
@@ -34,35 +30,86 @@ def test_abc_params_setter():
     assert new_abc.n_trials == 10
 
 
-def test_abc_build():
+def test_abc_create_additional_attrs():
+    search_space = search.SearchSpace(n_agents=10, n_variables=2,
+                                      lower_bound=[0, 0], upper_bound=[10, 10])
+
     new_abc = abc.ABC()
+    new_abc.create_additional_attrs(search_space)
 
-    assert new_abc.built == True
+    try:
+        new_abc.trial = 1
+    except:
+        new_abc.trial = np.array([1])
+
+    assert new_abc.trial == np.array([1])
 
 
-def test_abc_run():
+def test_abc_evaluate_location():
     def square(x):
         return np.sum(x**2)
 
-    def hook(optimizer, space, function):
-        return
+    search_space = search.SearchSpace(n_agents=10, n_variables=2,
+                                      lower_bound=[0, 0], upper_bound=[10, 10])
 
-    new_function = function.Function(pointer=square)
+    new_abc = abc.ABC()
+    new_abc.create_additional_attrs(search_space)
 
-    params = {
-        'n_trials': 1
-    }
+    new_abc._evaluate_location(
+        search_space.agents[0], search_space.agents[1], square, 0)
 
-    new_abc = abc.ABC(params=params)
 
-    search_space = search.SearchSpace(n_agents=10, n_iterations=100,
-                                      n_variables=2, lower_bound=[0, 0],
-                                      upper_bound=[10, 10])
+def test_abc_send_employee():
+    def square(x):
+        return np.sum(x**2)
 
-    history = new_abc.run(search_space, new_function, pre_evaluate=hook)
+    search_space = search.SearchSpace(n_agents=10, n_variables=2,
+                                      lower_bound=[0, 0], upper_bound=[10, 10])
 
-    assert len(history.agents) > 0
-    assert len(history.best_agent) > 0
+    new_abc = abc.ABC()
+    new_abc.create_additional_attrs(search_space)
 
-    best_fitness = history.best_agent[-1][1]
-    assert best_fitness <= constant.TEST_EPSILON, 'The algorithm abc failed to converge.'
+    new_abc._send_employee(search_space.agents, square)
+
+
+def test_abc_send_onlooker():
+    def square(x):
+        return np.sum(x**2)
+
+    search_space = search.SearchSpace(n_agents=10, n_variables=2,
+                                      lower_bound=[0, 0], upper_bound=[10, 10])
+
+    new_abc = abc.ABC()
+    new_abc.create_additional_attrs(search_space)
+
+    new_abc._send_onlooker(search_space.agents, square)
+
+
+def test_abc_send_scout():
+    def square(x):
+        return np.sum(x**2)
+
+    search_space = search.SearchSpace(n_agents=10, n_variables=2,
+                                      lower_bound=[0, 0], upper_bound=[10, 10])
+
+    new_abc = abc.ABC()
+    new_abc.create_additional_attrs(search_space)
+
+    new_abc._send_scout(search_space.agents, square)
+
+    new_abc.trial[0] = 5
+    new_abc.n_trials = 1
+    new_abc._send_scout(search_space.agents, square)
+
+
+def test_abc_update():
+    def square(x):
+        return np.sum(x**2)
+
+    search_space = search.SearchSpace(n_agents=10, n_variables=2,
+                                      lower_bound=[0, 0], upper_bound=[10, 10])
+
+    new_abc = abc.ABC()
+    new_abc.create_additional_attrs(search_space)
+
+    new_abc.update(search_space, square)
