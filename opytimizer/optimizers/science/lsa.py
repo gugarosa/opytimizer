f7555bcfc055662dbc155a2df8c0d8ed77cfb83d
Gustavo Rosa <gth.rosa@uol.com.br>
2021-05-14 15:17:44 -0300

    Finishing up LSA and its tests.

--- opytimizer/optimizers/science/lsa.py
+++ opytimizer/optimizers/science/lsa.py
@@ -4,12 +4,9 @@
 import copy
 
 import numpy as np
-from tqdm import tqdm
 
 import opytimizer.math.random as r
-import opytimizer.utils.constant as c
 import opytimizer.utils.exception as e
-import opytimizer.utils.history as h
 import opytimizer.utils.logging as l
 from opytimizer.core.optimizer import Optimizer
 
@@ -41,13 +38,13 @@ class LSA(Optimizer):
         # Overrides its parent class with the receiving params
         super(LSA, self).__init__()
 
-        #
+        # Maximum channel time
         self.max_time = 10
 
-        #
-        self.init_energy = 2.05
+        # Initial energy
+        self.E = 2.05
 
-        #
+        # Forking probability
         self.p_fork = 0.01
 
         # Builds the class
@@ -55,71 +52,237 @@ class LSA(Optimizer):
 
         logger.info('Class overrided.')
 
+    @property
+    def max_time(self):
+        """int: Maximum channel time.
+
+        """
+
+        return self._max_time
+
+    @max_time.setter
+    def max_time(self, max_time):
+        if not isinstance(max_time, int):
+            raise e.TypeError('`max_time` should be an integer')
+        if max_time <= 0:
+            raise e.ValueError('`max_time` should be > 0')
+
+        self._max_time = max_time
+
+    @property
+    def E(self):
+        """float: Initial energy.
+
+        """
+
+        return self._E
+
+    @E.setter
+    def E(self, E):
+        if not isinstance(E, (float, int)):
+            raise e.TypeError('`E` should be a float or integer')
+        if E < 0:
+            raise e.ValueError('`E` should be >= 0')
+
+        self._E = E
+
+    @property
+    def p_fork(self):
+        """float: Probability of forking.
+
+        """
+
+        return self._p_fork
+
+    @p_fork.setter
+    def p_fork(self, p_fork):
+        if not isinstance(p_fork, (float, int)):
+            raise e.TypeError('`p_fork` should be a float or integer')
+        if p_fork < 0 or p_fork > 1:
+            raise e.ValueError('`p_fork` should be between 0 and 1')
+
+        self._p_fork = p_fork
+
+    @property
+    def time(self):
+        """int: Channel time.
+
+        """
+
+        return self._time
+
+    @time.setter
+    def time(self, time):
+        if not isinstance(time, int):
+            raise e.TypeError('`time` should be an integer')
+        if time < 0:
+            raise e.ValueError('`time` should be >= 0')
+
+        self._time = time
+
+    @property
+    def direction(self):
+        """np.array: Array of directions.
+
+        """
+
+        return self._direction
+
+    @direction.setter
+    def direction(self, direction):
+        if not isinstance(direction, np.ndarray):
+            raise e.TypeError('`direction` should be a numpy array')
+
+        self._direction = direction
+
     def create_additional_attrs(self, space):
+        """Creates additional attributes that are used by this optimizer.
+
+        Args:
+            space (Space): A Space object containing meta-information.
+
+        """
+
+        # Channel time
         self.time = 0
-        self.direction = np.sign(
-            r.generate_uniform_random_number(-1, 1, space.n_variables))
+
+        # Array of directions
+        self.direction = np.sign(r.generate_uniform_random_number(-1, 1, (space.n_variables, space.n_dimensions)))
+
+    def _update_direction(self, agent, function):
+        """Updates the direction array by shaking agent's direction.
+
+        Args:
+            agent (Agent): An agent instance.
+            function (Function): A Function object that will be used as the objective function.
+
+        """
+
+        # Iterates through all decision variables
+        for j in range(agent.n_variables):
+            # Makes a copy of agent
+            direction = copy.deepcopy(agent)
+
+            # Shakes the direction
+            direction.position[j] += self.direction[j] * 0.005 * (agent.ub[j] - agent.lb[j])
+
+            # Clips its bounds
+            direction.clip_by_bound()
+
+            # Evaluates the direction
+            direction.fit = function(direction.position)
+
+            # If new direction's fitness is worst than agent's fitness
+            if direction.fit > agent.fit:
+                # Inverts the direction
+                self.direction[j] *= -1
+
+    def _update_position(self, agent, best_agent, function, energy):
+        """Updates agent's position.
+
+        Args:
+            agent (Agent): An agent instance.
+            best_agent (Agent): A best agent instance.
+            function (Function): A Function object that will be used as the objective function.
+            energy (float): Current energy value.
+
+        """
+
+        # Makes a copy of agent
+        a = copy.deepcopy(agent)
+
+        # Calculates the distance between agent and best agent
+        distance = agent.position - best_agent.position
+
+        # Iterates through all decision variables
+        for j in range(agent.n_variables):
+            # Iterates through all dimensions
+            for k in range(agent.n_dimensions):
+                # If distance equals to zero
+                if distance[j][k] == 0:
+                    # Updates the position by sampling a gaussian number
+                    r1 = r.generate_gaussian_random_number(0, energy)
+                    a.position[j][k] += self.direction[j][k] * r1
+
+                # If distance is different from zero
+                else:
+                    # If distance is smaller than zero
+                    if distance[j][k] < 0:
+                        # Updates the position by adding an exponential number
+                        a.position[j][k] += r.generate_exponential_random_number(np.fabs(distance[j][k]))
+
+                    # If distance is bigger than zero
+                    else:
+                        # Updates the position by subtracting an exponential number
+                        a.position[j][k] -= r.generate_exponential_random_number(distance[j][k])
+
+        # Clips the temporary agent's limits
+        a.clip_by_bound()
+
+        # Evaluates its new position
+        a.fit = function(a.position)
+
+        # If temporary agent's fitness is better than current agent's fitness
+        if a.fit < agent.fit:
+            # Replaces position and fitness
+            agent.position = copy.deepcopy(a.position)
+            agent.fit = copy.deepcopy(a.fit)
+
+            # Generates a random number
+            r1 = r.generate_uniform_random_number()
+
+            # If random number is smaller than probability of forking
+            if r1 < self.p_fork:
+                # Makes a new copy of current agent
+                a = copy.deepcopy(agent)
+
+                # Generates a random position
+                a.fill_with_uniform()
+
+                # Re-evaluates its position
+                a.fit = function(a.position)
+
+                # If new fitness is better than agent's fitness
+                if a.fit < agent.fit:
+                    # Replaces position and fitness
+                    agent.position = copy.deepcopy(a.position)
+                    agent.fit = copy.deepcopy(a.fit)
 
     def update(self, space, function, iteration, n_iterations):
         """Wraps Lightning Search Algorithm over all agents and variables.
 
         Args:
             space (Space): Space containing agents and update-related information.
+            function (Function): A Function object that will be used as the objective function.
             iteration (int): Current iteration.
+            n_iterations (int): Maximum number of iterations.
 
         """
 
-        #
+        # Increases the channel's time
         self.time += 1
 
+        # If channel has reached maximum allowed time
         if self.time >= self.max_time:
+            # Sorts the agents
             space.agents.sort(key=lambda x: x.fit)
+
+            # Replaces the worst channel with the best one
             space.agents[-1] = copy.deepcopy(space.agents[0])
+
+            # Resets the channel's time
             self.time = 0
 
+        # Re-sorts the agents
         space.agents.sort(key=lambda x: x.fit)
 
-        best = space.agents[0].fit
+        # Updates the direction
+        self._update_direction(space.agents[0], function)
 
-        energy = self.init_energy - 2 * \
-            np.exp(-5 * (n_iterations - iteration) / n_iterations)
+        # Calculates the current energy
+        energy = self.E - 2 * np.exp(-5 * (n_iterations - iteration) / n_iterations)
 
-        for j in range(space.n_variables):
-            direction = space.agents[0].position[j] + \
-                self.direction[j] * 0.005 * (space.ub[j] - space.lb[j])
-            direction_fit = function(direction)
-            if direction_fit > best:
-                self.direction[j] *= -1
-
+        # Iterates through all agents
         for agent in space.agents:
-            temp = copy.deepcopy(agent)
-            distance = agent.position - space.agents[0].position
-            for j in range(agent.n_variables):
-                if agent.position[j] == space.agents[0].position[j]:
-                    temp.position[j] += self.direction[j] * np.fabs(r.generate_gaussian_random_number(0, energy))
-                else:
-                    if distance[j] < 0:
-                        temp.position[j] += r.generate_exponential_random_number(np.fabs(distance[j]))
-                    else:
-                        temp.position[j] -= r.generate_exponential_random_number(distance[j])
-
-            temp.clip_by_bound()
-
-            temp.fit = function(temp.position)
-
-            if temp.fit < agent.fit:
-                agent.position = copy.deepcopy(temp.position)
-                agent.fit = copy.deepcopy(temp.fit)
-
-                r1 = r.generate_uniform_random_number()
-
-                if r1 < self.p_fork:
-                    temp = copy.deepcopy(agent)
-                    for j in range(agent.n_variables):
-                        temp.position[j] = temp.ub[j] + \
-                            temp.lb[j] - temp.position[j]
-                    temp.fit = function(temp.position)
-
-                    if temp.fit < agent.fit:
-                        agent.position = copy.deepcopy(temp.position)
-                        agent.fit = copy.deepcopy(temp.fit)
+            # Updates agent's position
+            self._update_position(agent, space.agents[0], function, energy)
---
+++ tests/opytimizer/optimizers/science/test_lsa.py
@@ -0,0 +1,137 @@
+import numpy as np
+
+from opytimizer.optimizers.science import lsa
+from opytimizer.spaces import search
+
+
+def test_lsa_params():
+    params = {
+        'max_time': 10,
+        'E': 2.05,
+        'p_fork': 0.01
+    }
+
+    new_lsa = lsa.LSA(params=params)
+
+    assert new_lsa.max_time == 10
+    assert new_lsa.E == 2.05
+    assert new_lsa.p_fork == 0.01
+
+
+def test_lsa_params_setter():
+    new_lsa = lsa.LSA()
+
+    try:
+        new_lsa.max_time = 'a'
+    except:
+        new_lsa.max_time = 10
+
+    assert new_lsa.max_time == 10
+
+    try:
+        new_lsa.max_time = -1
+    except:
+        new_lsa.max_time = 10
+
+    assert new_lsa.max_time == 10
+
+    try:
+        new_lsa.E = 'b'
+    except:
+        new_lsa.E = 2.05
+
+    assert new_lsa.E == 2.05
+
+    try:
+        new_lsa.E = -1
+    except:
+        new_lsa.E = 2.05
+
+    assert new_lsa.E == 2.05
+
+    try:
+        new_lsa.p_fork = 'c'
+    except:
+        new_lsa.p_fork = 0.01
+
+    assert new_lsa.p_fork == 0.01
+
+    try:
+        new_lsa.p_fork = -1
+    except:
+        new_lsa.p_fork = 0.01
+
+    assert new_lsa.p_fork == 0.01
+
+
+def test_lsa_create_additional_attrs():
+    search_space = search.SearchSpace(n_agents=10, n_variables=2,
+                                      lower_bound=[0, 0], upper_bound=[10, 10])
+
+    new_lsa = lsa.LSA()
+    new_lsa.create_additional_attrs(search_space)
+
+    try:
+        new_lsa.time = 'a'
+    except:
+        new_lsa.time = 0
+
+    assert new_lsa.time == 0
+
+    try:
+        new_lsa.time = -1
+    except:
+        new_lsa.time = 0
+
+    assert new_lsa.time == 0
+
+    try:
+        new_lsa.direction = 1
+    except:
+        new_lsa.direction = np.array([1])
+
+    assert new_lsa.direction == np.array([1])
+
+
+def test_lsa_update_direction():
+    def square(x):
+        return np.sum(x**2)
+
+    search_space = search.SearchSpace(n_agents=10, n_variables=2,
+                                      lower_bound=[0, 0], upper_bound=[10, 10])
+
+    new_lsa = lsa.LSA()
+    new_lsa.create_additional_attrs(search_space)
+
+    new_lsa._update_direction(search_space.agents[0], square)
+
+
+def test_lsa_update_position():
+    def square(x):
+        return np.sum(x**2)
+
+    search_space = search.SearchSpace(n_agents=10, n_variables=2,
+                                      lower_bound=[0, 0], upper_bound=[10, 10])
+
+    new_lsa = lsa.LSA()
+    new_lsa.create_additional_attrs(search_space)
+
+    new_lsa._update_position(
+        search_space.agents[0], search_space.agents[0], square, 0.5)
+
+
+def test_lsa_update():
+    def square(x):
+        return np.sum(x**2)
+
+    search_space = search.SearchSpace(n_agents=10, n_variables=2,
+                                      lower_bound=[0, 0], upper_bound=[10, 10])
+
+    new_lsa = lsa.LSA()
+    new_lsa.create_additional_attrs(search_space)
+
+    new_lsa.p_fork = 1
+    new_lsa.update(search_space, square, 1, 10)
+
+    new_lsa.time = 11
+    new_lsa.update(search_space, square, 1, 10)
