02f1f31ff0d31b1e35e25daa2144de32d0da3b77
Gustavo Rosa <gth.rosa@uol.com.br>
2021-05-14 15:19:20 -0300

    Forgot to add the params on create_lsa script.

--- examples/optimizers/science/create_lsa.py
+++ examples/optimizers/science/create_lsa.py
@@ -3,7 +3,9 @@ from opytimizer.optimizers.science.lsa import LSA
 # One should declare a hyperparameters object based
 # on the desired algorithm that will be used
 params = {
-
+    'max_time': 10,
+    'E': 2.05,
+    'p_fork': 0.01
 }
 
 # Creates an LSA optimizer
