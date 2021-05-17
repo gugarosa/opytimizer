f372ee56e53019182b42332e46c3ccc6f17758fb
Gustavo Rosa <gth.rosa@uol.com.br>
2021-05-17 12:22:48 -0300

    Still need to address some convergence issues.

--- opytimizer/optimizers/science/moa.py
+++ opytimizer/optimizers/science/moa.py
@@ -1,16 +1,12 @@
 """Magnetic Optimization Algorithm.
 """
 
-import copy
-
 import numpy as np
-from tqdm import tqdm
 
 import opytimizer.math.general as g
 import opytimizer.math.random as r
 import opytimizer.utils.constant as c
 import opytimizer.utils.exception as e
-import opytimizer.utils.history as h
 import opytimizer.utils.logging as l
 from opytimizer.core.optimizer import Optimizer
 
@@ -42,10 +38,10 @@ class MOA(Optimizer):
         # Overrides its parent class with the receiving params
         super(MOA, self).__init__()
 
-        #
+        # Particle moviment first constant
         self.alpha = 1
 
-        #
+        # Particle moviment second constant
         self.rho = 2
 
         # Builds the class
@@ -53,59 +49,118 @@ class MOA(Optimizer):
 
         logger.info('Class overrided.')
 
+    @property
+    def alpha(self):
+        """float: Particle moviment first constant.
+
+        """
+
+        return self._alpha
+
+    @alpha.setter
+    def alpha(self, alpha):
+        if not isinstance(alpha, (float, int)):
+            raise e.TypeError('`alpha` should be a float or integer')
+        if alpha < 0:
+            raise e.ValueError('`alpha` should be >= 0')
+
+        self._alpha = alpha
+
+    @property
+    def rho(self):
+        """float: Particle moviment second constant.
+
+        """
+
+        return self._rho
+
+    @rho.setter
+    def rho(self, rho):
+        if not isinstance(rho, (float, int)):
+            raise e.TypeError('`rho` should be a float or integer')
+        if rho < 0:
+            raise e.ValueError('`rho` should be >= 0')
+
+        self._rho = rho
+
+    @property
+    def velocity(self):
+        """np.array: Array of velocities.
+
+        """
+
+        return self._velocity
+
+    @velocity.setter
+    def velocity(self, velocity):
+        if not isinstance(velocity, np.ndarray):
+            raise e.TypeError('`velocity` should be a numpy array')
+
+        self._velocity = velocity
+
     def create_additional_attrs(self, space):
-        #
+        """Creates additional attributes that are used by this optimizer.
+
+        Args:
+            space (Space): A Space object containing meta-information.
+
+        """
+
+        # Checks if supplied number of agents has a perfect square
         if not np.sqrt(space.n_agents).is_integer():
             raise e.SizeError('`n_agents` should have a perfect square')
 
+        # Array of velocities
         self.velocity = np.zeros((space.n_agents, space.n_variables, space.n_dimensions))
 
     def update(self, space):
-        #
+        """Wraps Magnetic Optimization Algorithm over all agents and variables.
+
+        Args:
+            space (Space): Space containing agents and update-related information.
+            iteration (int): Current iteration.
+
+        """
+
+        # Sorts the agents
         space.agents.sort(key=lambda x: x.fit)
 
-        # (eq. 2)
+        # Gathers the best and worst agents and calculates a list of normalized fitness (eq. 2)
         best, worst = space.agents[0], space.agents[-1]
-        fitness = [(agent.fit - best.fit) / (worst.fit - best.fit + c.EPSILON) for agent in space.agents]
+        fitness = [(agent.fit - best.fit) / (worst.fit - best.fit + c.EPSILON)
+                   for agent in space.agents]
 
-        print([agent.fit for agent in space.agents])
-
-        # (eq. 3)
+        # Calculates the masses (eq. 3)
         mass = [self.alpha + self.rho * fit for fit in fitness]
 
-        #
+        # Iterates through all agents
         for i, agent in enumerate(space.agents):
-            # (eq. 4)
+            # Gathers the agents neighbours (eq. 4)
             root = np.sqrt(space.n_agents)
             north = int((i - root) % space.n_agents)
             south = int((i + root) % space.n_agents)
             west = int((i - 1) + ((i + root - 1) % root) // (root - 1) * root)
             east = int((i + 1) - (i % root) // (root - 1) * root)
-
-            #
             neighbours = [north, south, west, east]
 
-            #
+            # Initializes the force as a zero value
             force = 0
 
-            #
+            # Iterates through all neighbours
             for n in neighbours:
-                # (eq. 7)
-                distance = g.euclidean_distance(agent.position, space.agents[n].position)
+                # Calculates the distance between current agent and neighbour (eq. 7)
+                distance = g.euclidean_distance(
+                    agent.position, space.agents[n].position)
 
-                # (eq. 5)
+                # Calculates the force between agents (eq. 5)
                 force += (space.agents[n].position - agent.position) * fitness[n] / (distance + c.EPSILON)
 
-            # print(force, mass[i])
-
-            # (eq. 11)
-            r1 = r.generate_uniform_random_number(-10, 10, size=(space.n_variables, space.n_dimensions))
+            # Calculates the acceleration (eq. 11)
+            r1 = r.generate_uniform_random_number(size=(space.n_variables, space.n_dimensions))
             acceleration = (force / mass[i]) * r1
 
-            # (eq. 12 - top)
+            # Updates the agent's velocity(eq. 12 - top)
             self.velocity[i] += acceleration
 
-            # print(force, mass, velocity)
-
-            # (eq. 12 - bottom)
+            # Updates the agent's position (eq. 12 - bottom)
             agent.position += self.velocity[i]
