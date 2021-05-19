48c2857e7b25f7c12918f3a6f643d86cfb0a2e3f
Gustavo Rosa <gth.rosa@uol.com.br>
2021-05-19 10:33:06 -0300

    Starting to implement the draft of Parasitism-Predation Algorithm.

--- examples/applications/single_objective/standard_optimization.py
+++ examples/applications/single_objective/standard_optimization.py
@@ -3,6 +3,7 @@ from opytimark.markers.n_dimensional import Sphere
 
 from opytimizer import Opytimizer
 from opytimizer.core import Function
+from opytimizer.optimizers.population import PPA
 from opytimizer.optimizers.swarm import PSO
 from opytimizer.spaces import SearchSpace
 
@@ -11,19 +12,19 @@ np.random.seed(0)
 
 # Number of agents and decision variables
 n_agents = 20
-n_variables = 2
+n_variables = 200
 
 # Lower and upper bounds (has to be the same size as `n_variables`)
-lower_bound = [-10, -10]
-upper_bound = [10, 10]
+lower_bound = np.full(200, -10)
+upper_bound = np.full(200, 10)
 
 # Creates the space, optimizer and function
 space = SearchSpace(n_agents, n_variables, lower_bound, upper_bound)
-optimizer = PSO()
+optimizer = PPA()
 function = Function(Sphere())
 
 # Bundles every piece into Opytimizer class
 opt = Opytimizer(space, optimizer, function, save_agents=False)
 
 # Runs the optimization task
-opt.start(n_iterations=10000)
+opt.start(n_iterations=1000)
--- opytimizer/optimizers/population/ppa.py
+++ opytimizer/optimizers/population/ppa.py
@@ -6,6 +6,8 @@ import copy
 import numpy as np
 from tqdm import tqdm
 
+import opytimizer.math.distribution as d
+import opytimizer.math.general as g
 import opytimizer.math.random as r
 import opytimizer.utils.constant as c
 import opytimizer.utils.exception as e
@@ -41,7 +43,122 @@ class PPA(Optimizer):
         # Overrides its parent class with the receiving params
         super(PPA, self).__init__()
 
+        #
+        self.r1 = 1
+
+        #
+        self.r2 = 0.1
+
+        #
+        self.r3 = 0.1
+
+        #
+        self.alpha1 = 0.2
+
+        #
+        self.alpha2 = 0.25
+
+        #
+        self.beta1 = 0.1
+
+        #
+        self.beta2 = 0.1
+
+        #
+        self.c1 = 0.1
+
+        #
+        self.c2 = 0.1
+
+        #
+        self.d1 = 0.01
+
+        #
+        self.d2 = 0.01
+
         # Builds the class
         self.build(params)
 
         logger.info('Class overrided.')
+
+    def compile(self, space):
+        self.velocity = np.zeros((space.n_agents, space.n_variables, space.n_dimensions))
+
+    def _growth_rate(self, n_agents, iteration, n_iterations):
+        """
+        """
+
+        #
+        n_crows = np.round(n_agents * (2/3 - iteration * ((2/3 - 1/2) / n_iterations)))
+
+        #
+        n_cats = np.round(n_agents * (0.01 + iteration * ((1/3 - 0.01) / n_iterations)))
+
+        #
+        n_cuckoos = n_agents - n_crows - n_cats
+
+        return int(n_crows), int(n_cats), int(n_cuckoos)
+
+    def update(self, space, iteration, n_iterations):
+        #
+        n_crows, n_cats, n_cuckoos = self._growth_rate(space.n_agents, iteration, n_iterations)
+
+        # print(n_crows, n_cats, n_cuckoos)
+
+        #
+        for i, agent in enumerate(space.agents[:n_crows]):
+            #
+            idx = r.generate_integer_random_number(high=space.n_agents, exclude_value=i)
+
+            # (eq. 7)
+            step = d.generate_levy_distribution(size=agent.n_variables)
+            step = np.expand_dims(step, axis=1)
+
+            # (eq. 6 and 8)
+            agent.position = 0.01 * step * (space.agents[idx].position - agent.position)
+
+            #
+            agent.clip_by_bound()
+
+        #
+        p = iteration / n_iterations
+
+        # Calculates a list of current trees' fitness
+        fitness = [agent.fit for agent in space.agents[n_crows:n_crows+n_cuckoos]]
+
+        #
+        for agent in space.agents[n_crows:n_crows+n_cuckoos]:
+            #
+            s = g.tournament_selection(fitness, 1)[0]
+
+            #
+            i = r.generate_integer_random_number(0, space.n_agents)
+            j = r.generate_integer_random_number(0, space.n_agents, exclude_value=i)
+
+            # (eq. 12)
+            k = r.generate_uniform_random_number(size=agent.n_variables) > p
+            k = np.expand_dims(k, -1)
+
+            # (eq. 11)
+            rand = r.generate_uniform_random_number()
+            S_g = (space.agents[i].position - space.agents[j].position) * rand
+
+            # (eq. 10)
+            agent.position = space.agents[s].position + S_g * k
+
+            agent.clip_by_bound()
+            
+
+        #
+        _c = 2 - iteration / n_iterations
+
+        #
+        for i, agent in enumerate(space.agents[n_crows+n_cuckoos:]):
+            idx = i + n_crows + n_cuckoos
+            r1 = r.generate_uniform_random_number()
+            self.velocity[idx] += r1 * _c * (space.best_agent.position - agent.position)
+
+            agent.position += self.velocity[idx]
+
+            agent.clip_by_bound()
+
