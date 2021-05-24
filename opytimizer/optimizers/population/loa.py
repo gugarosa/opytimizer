16e70c71120f7a1fe3107937480a84985e38b411
Gustavo Rosa <gth.rosa@uol.com.br>
2021-05-24 09:51:16 -0300

    Starting to implement the Lion Optimization Algorithm. This will take a time.

--- opytimizer/optimizers/population/loa.py
+++ opytimizer/optimizers/population/loa.py
@@ -41,7 +41,46 @@ class LOA(Optimizer):
         # Overrides its parent class with the receiving params
         super(LOA, self).__init__()
 
+        #
+        self.N = 0.5
+
+        #
+        self.P = 2
+
+        #
+        self.S = 0.75
+
         # Builds the class
         self.build(params)
 
         logger.info('Class overrided.')
+
+    def compile(self, space):
+        self.n_nomad = int(self.N * space.n_agents)
+        self.n_pride = (space.n_agents - self.n_nomad) // self.P
+
+        self.gender = [1] * space.n_agents
+
+    def _get_nomad_lions(self, agents):
+        return agents[:self.n_nomad], self.gender[:self.n_nomad]
+
+    def _get_pride_lions(self, agents):
+        prides, genders = [], []
+        for i in range(self.P):
+            start, end = i * self.n_pride, (i + 1) * self.n_pride
+            prides.append(agents[start:end])
+            genders.append(self.gender[start:end])
+        return prides, genders
+
+    def _hunting(self, prides, genders):
+        for pride, gender in zip(prides, genders):
+            for p, g in zip(pride, gender):
+                if g == 0:
+                    pass
+
+
+    def update(self, space):
+        p, g = self._get_pride_lions(space.agents)
+
+        self._hunting(p, g)
+
