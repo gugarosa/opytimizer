1f48b80b00ac888cda97e02c89732c113e426fbb
Gustavo Rosa <gth.rosa@uol.com.br>
2021-05-24 12:25:40 -0300

    Adding Roaming() method on LOA.

--- opytimizer/optimizers/population/loa.py
+++ opytimizer/optimizers/population/loa.py
@@ -60,13 +60,16 @@ class LOA(Optimizer):
         super(LOA, self).__init__()
 
         #
-        self.N = 0.5
+        self.N = 0.2
 
         #
-        self.P = 2
+        self.P = 4
 
         #
-        self.S = 0.75
+        self.S = 0.8
+
+        #
+        self.R = 0.2
 
         # Builds the class
         self.build(params)
@@ -179,6 +182,49 @@ class LOA(Optimizer):
                     R2 = R2.T - R2.dot(R1) * R1 / (np.linalg.norm(R1) ** 2 + c.EPSILON)
                     agent.position += 2 * distance * r1 * R1 + u * np.tan(theta) * R2 * distance
 
+    def _roaming(self, prides, function):
+        for pride in prides:
+            # probs = [self.P] * len(pride)
+            n_territory = int(len(pride) * self.P)
+            choices = r.generate_integer_random_number(high=len(pride), size=n_territory)
+            # print(choices)
+            for agent in pride:
+                if agent.female is not True:
+                    for choice in choices:
+                        angle = np.random.uniform(-np.pi/6, np.pi/6)
+                        distance = g.euclidean_distance(pride[choice].best_position, agent.position)
+                        step = r.generate_uniform_random_number(0, 2 * distance)
+                        agent.position += step * np.tan(angle)
+                        agent.clip_by_bound()
+
+                        agent.p_fit = copy.deepcopy(agent.fit)
+                        agent.fit = function(agent.position)
+
+                        if agent.fit < agent.p_fit:
+                            agent.best_position = copy.deepcopy(agent.position)
+
+    # def evaluate(self, space, function):
+    #     # Iterates through all agents
+    #     for i, agent in enumerate(space.agents):
+    #         # Calculates the fitness value of current agent
+    #         fit = function(agent.best_position)
+
+    #         # If fitness is better than agent's best fit
+    #         if fit < agent.fit:
+    #             # Updates its current fitness to the newer one
+    #             agent.fit = fit
+
+    #             # Also updates the local best position to current's agent position
+    #             # agent.best_position[i] = copy.deepcopy(agent.position)
+
+    #         # If agent's fitness is better than global fitness
+    #         if agent.fit < space.best_agent.fit:
+    #             # Makes a deep copy of agent's local best position and fitness to the best agent
+    #             space.best_agent.position = copy.deepcopy(agent.best_position)
+    #             space.best_agent.fit = copy.deepcopy(agent.fit)
+    
+
+
     def update(self, space, function):
         # print(self._get_nomad_lions(space.agents))
         prides = self._get_pride_lions(space.agents)
@@ -186,6 +232,8 @@ class LOA(Optimizer):
         self._hunting(prides, function)
 
         self._moving_safe_place(prides)
+
+        self._roaming(prides, function)
 
         # for agent in space.agents:
         #     print(agent.group)
