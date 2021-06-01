95817bd6da4cb250f2150794e17f4e88dd5052b6
Gustavo Rosa <gth.rosa@uol.com.br>
2021-06-01 15:04:36 -0300

    Starting to add Artificial Flora.

--- opytimizer/optimizers/swarm/af.py
+++ opytimizer/optimizers/swarm/af.py
@@ -41,7 +41,80 @@ class AF(Optimizer):
         # Overrides its parent class with the receiving params
         super(AF, self).__init__()
 
+        #
+        self.c1 = 0.75
+
+        #
+        self.c2 = 1.25
+
+        # Amount of branches
+        self.m = 10
+
+        #
+        self.Q = 0.75
+
         # Builds the class
         self.build(params)
 
         logger.info('Class overrided.')
+
+    def compile(self, space):
+        self.p_distance = r.generate_uniform_random_number(size=space.n_agents)
+        self.g_distance = r.generate_uniform_random_number(size=space.n_agents)
+
+    def update(self, space, function):
+
+        #
+        space.agents.sort(key=lambda x: x.fit)
+
+        #
+        new_agents = []
+
+        for i, agent in enumerate(space.agents):
+            #
+            for _ in range(self.m):
+                #
+                a = copy.deepcopy(agent)
+
+                #
+                r1 = r.generate_uniform_random_number()
+                r2 = r.generate_uniform_random_number()
+
+                #
+                distance = self.g_distance[i] * r1 * \
+                    self.c1 + self.p_distance[i] * r2 * self.c2
+
+                # print(distance)
+
+                #
+                D = r.generate_gaussian_random_number(variance=distance)
+
+                a.position += D
+                a.clip_by_bound()
+
+                #
+                a.fit = function(a.position)
+
+                # print(a.fit)
+
+                # print(D)
+
+                #
+                p = np.fabs(np.sqrt(a.fit / space.agents[-1].fit)) * self.Q
+
+                #
+                r3 = r.generate_uniform_random_number()
+
+                if p < r3:
+                    new_agents.append(a)
+
+            #
+            self.g_distance[i] = self.p_distance[i]
+            self.p_distance[i] = np.sqrt(
+                np.sum((agent.position - a.position) ** 2) / space.n_agents)
+
+            print(self.g_distance[i], self.p_distance[i])
+
+        space.agents += new_agents
+        space.agents.sort(key=lambda x: x.fit)
+        space.agents = space.agents[:space.n_agents]
