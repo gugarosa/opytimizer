348101e2af91df7d047102aa680a9045d9e93102
Gustavo Rosa <gth.rosa@uol.com.br>
2021-06-02 17:26:35 -0300

    Continuing to implement AF.

--- examples/optimizers/swarm/create_af.py
+++ examples/optimizers/swarm/create_af.py
@@ -3,7 +3,10 @@ from opytimizer.optimizers.swarm import AF
 # One should declare a hyperparameters object based
 # on the desired algorithm that will be used
 params = {
-    
+    'c1': 0.75,
+    'c2': 1.25,
+    'm': 10,
+    'Q': 0.75
 }
 
 # Creates an AF optimizer
--- opytimizer/optimizers/swarm/af.py
+++ opytimizer/optimizers/swarm/af.py
@@ -4,12 +4,10 @@
 import copy
 
 import numpy as np
-from tqdm import tqdm
 
 import opytimizer.math.random as r
 import opytimizer.utils.constant as c
 import opytimizer.utils.exception as e
-import opytimizer.utils.history as h
 import opytimizer.utils.logging as l
 from opytimizer.core import Optimizer
 
@@ -58,25 +56,111 @@ class AF(Optimizer):
 
         logger.info('Class overrided.')
 
+    @property
+    def c1(self):
+        """float: First learning coefficient.
+
+        """
+
+        return self._c1
+
+    @c1.setter
+    def c1(self, c1):
+        if not isinstance(c1, (float, int)):
+            raise e.TypeError('`c1` should be a float or integer')
+        if c1 < 0:
+            raise e.ValueError('`c1` should be >= 0')
+
+        self._c1 = c1
+
+    @property
+    def c2(self):
+        """float: Second learning coefficient.
+
+        """
+
+        return self._c2
+
+    @c2.setter
+    def c2(self, c2):
+        if not isinstance(c2, (float, int)):
+            raise e.TypeError('`c2` should be a float or integer')
+        if c2 < 0:
+            raise e.ValueError('`c2` should be >= 0')
+
+        self._c2 = c2
+
+    @property
+    def m(self):
+        """int: Amount of branches.
+
+        """
+
+        return self._m
+
+    @m.setter
+    def m(self, m):
+        if not isinstance(m, int):
+            raise e.TypeError('`m` should be an integer')
+        if m <= 0:
+            raise e.ValueError('`m` should be > 0')
+
+        self._m = m
+
+    @property
+    def Q(self):
+        """float: Selective probability.
+
+        """
+
+        return self._Q
+
+    @Q.setter
+    def Q(self, Q):
+        if not isinstance(Q, (float, int)):
+            raise e.TypeError('`Q` should be a float or integer')
+        if Q < 0 or Q > 1:
+            raise e.ValueError('`Q` should be between 0 and 1')
+
+        self._Q = Q
+
     def compile(self, space):
+        """Compiles additional information that is used by this optimizer.
+
+        Args:
+            space (Space): A Space object containing meta-information.
+
+        """
+
+        # Array of parent distances
         self.p_distance = r.generate_uniform_random_number(size=space.n_agents)
+
+        # Array of grand-parent distances
         self.g_distance = r.generate_uniform_random_number(size=space.n_agents)
 
     def update(self, space, function):
+        """Wraps Artificial Flora over all agents and variables.
 
-        #
+        Args:
+            space (Space): Space containing agents and update-related information.
+            function (Function): A Function object that will be used as the objective function.
+
+        """
+        
+        # Sorts the agents
         space.agents.sort(key=lambda x: x.fit)
 
-        #
+        # Creates a list of new agents
         new_agents = []
 
+        # Iterates thorugh all agents
         for i, agent in enumerate(space.agents):
-            #
+            # Iterates through amount of branches
             for _ in range(self.m):
-                #
+                # Makes a copy of current agent
                 a = copy.deepcopy(agent)
 
-                #
+                # Generates random numbers
                 r1 = r.generate_uniform_random_number()
                 r2 = r.generate_uniform_random_number()
 
