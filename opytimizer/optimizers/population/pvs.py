3734ace94b9e3fe1fec48f3b7ad83af136949110
Gustavo Rosa <gth.rosa@uol.com.br>
2021-06-09 16:32:01 -0300

    Implementing the first draft of Passing Vehicle Search.

--- opytimizer/optimizers/population/pvs.py
+++ opytimizer/optimizers/population/pvs.py
@@ -45,3 +45,55 @@ class PVS(Optimizer):
         self.build(params)
 
         logger.info('Class overrided.')
+
+    def update(self, space, function):
+        #
+        space.agents.sort(key=lambda x: x.fit)
+
+        #
+        for i, agent in enumerate(space.agents):
+            a = copy.deepcopy(agent)
+
+            #
+            R = [0, 0]
+
+            #
+            while R[0] == R[1]:
+                #
+                R = r.generate_integer_random_number(0, space.n_agents, i, 2)
+
+            #
+            D1 = 1 / space.n_agents * agent.fit
+            D2 = 1 / space.n_agents * space.agents[R[0]].fit
+            D3 = 1 / space.n_agents * space.agents[R[1]].fit
+
+            #
+            V1 = r.generate_uniform_random_number() * (1 - D1)
+            V2 = r.generate_uniform_random_number() * (1 - D2)
+            V3 = r.generate_uniform_random_number() * (1 - D3)
+
+            #
+            x = np.fabs(D3 - D1)
+            y = np.fabs(D3 - D2)
+            x1 = (V3 * x) / (V1 - V3)
+            y1 = (V2 * x) / (V1 - V3)
+
+            r1 = r.generate_uniform_random_number()
+
+            #
+            if V3 < V1:
+                #
+                if (y - y1) > x1:
+                    Vco = V1 / (V1 - V3)
+                    a.position += Vco * r1 * (a.position - space.agents[R[1]].position)
+                else:
+                    a.position += r1 * (a.position - space.agents[R[0]].position)
+            else:
+                a.position += r1 * (space.agents[R[1]].position - a.position)
+            
+            a.clip_by_bound()
+            a.fit = function(a.position)
+
+            if a.fit < agent.fit:
+                agent.fit = copy.deepcopy(a.fit)
+                agent.position = copy.deepcopy(a.position)
