77f342dd199baf5f0efbcd06448532f804c43418
Gustavo Rosa <gth.rosa@uol.com.br>
2021-06-09 17:07:08 -0300

    Finishing up Passing Vehicle Search and its corresponding tests.

--- opytimizer/optimizers/population/pvs.py
+++ opytimizer/optimizers/population/pvs.py
@@ -4,12 +4,8 @@
 import copy
 
 import numpy as np
-from tqdm import tqdm
 
 import opytimizer.math.random as r
-import opytimizer.utils.constant as c
-import opytimizer.utils.exception as e
-import opytimizer.utils.history as h
 import opytimizer.utils.logging as l
 from opytimizer.core import Optimizer
 
@@ -47,53 +43,81 @@ class PVS(Optimizer):
         logger.info('Class overrided.')
 
     def update(self, space, function):
-        #
+        """Wraps Passing Vehicle Search over all agents and variables.
+
+        Args:
+            space (Space): Space containing agents and update-related information.
+            function (Function): A Function object that will be used as the objective function.
+
+        """
+
+        # Sorts agents
         space.agents.sort(key=lambda x: x.fit)
 
-        #
+        # Iterates through all agents
         for i, agent in enumerate(space.agents):
+            # Makes a deep copy of current agent
             a = copy.deepcopy(agent)
 
-            #
+            # Creates a list of equal indexes
             R = [0, 0]
 
-            #
+            # Keeps generating indexes until they are different
             while R[0] == R[1]:
-                #
+                # Generates random indexes
                 R = r.generate_integer_random_number(0, space.n_agents, i, 2)
 
-            #
+            # Calculates the selected agents distances (eq. 16)
             D1 = 1 / space.n_agents * agent.fit
             D2 = 1 / space.n_agents * space.agents[R[0]].fit
             D3 = 1 / space.n_agents * space.agents[R[1]].fit
 
-            #
+            # Calculates the selected agents velocities (eq. 17)
             V1 = r.generate_uniform_random_number() * (1 - D1)
             V2 = r.generate_uniform_random_number() * (1 - D2)
             V3 = r.generate_uniform_random_number() * (1 - D3)
 
-            #
+            # Calculates both `x` and `y` distance differences (eq. 18 and 19)
             x = np.fabs(D3 - D1)
             y = np.fabs(D3 - D2)
+
+            # Calculates both `x1` and `y1` constraints (eq. 4 and 7)
             x1 = (V3 * x) / (V1 - V3)
             y1 = (V2 * x) / (V1 - V3)
 
-            r1 = r.generate_uniform_random_number()
+            # Generates a random uniform number
+            rnd = r.generate_uniform_random_number()
 
-            #
+            # If third agent's velocity is smaller than first agent's velocity
             if V3 < V1:
-                #
+                # If difference between `y` gaps is bigger than `x1`
                 if (y - y1) > x1:
+                    # Calculates the condition velocity (eq. 23)
                     Vco = V1 / (V1 - V3)
-                    a.position += Vco * r1 * (a.position - space.agents[R[1]].position)
+
+                    # Updates the temporary agent's position accordingly (eq. 20)
+                    a.position += Vco * rnd * \
+                        (a.position - space.agents[R[1]].position)
+
+                # If difference between `y` gaps is smaller than `x1`
                 else:
-                    a.position += r1 * (a.position - space.agents[R[0]].position)
+                    # Updates the temporary agent's position accordingly (eq. 21)
+                    a.position += rnd * \
+                        (a.position - space.agents[R[0]].position)
+
+            # If third agent's velocity is bigger than first agent's velocity
             else:
-                a.position += r1 * (space.agents[R[1]].position - a.position)
-            
+                # Updates the temporary agent's position accordingly (eq. 22)
+                a.position += rnd * (space.agents[R[1]].position - a.position)
+
+            # Clips the temporary agent
             a.clip_by_bound()
+
+            # Re-evaluates its fitness
             a.fit = function(a.position)
 
+            # If temporary agent is better than old one
             if a.fit < agent.fit:
-                agent.fit = copy.deepcopy(a.fit)
+                # Copies its position and fitness to the agent
                 agent.position = copy.deepcopy(a.position)
+                agent.fit = copy.deepcopy(a.fit)
---
+++ tests/opytimizer/optimizers/population/test_pvs.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from opytimizer.optimizers.population import pvs
+from opytimizer.spaces import search
+
+
+def test_pvs_update():
+    def square(x):
+        return np.sum(x**2)
+
+    search_space = search.SearchSpace(n_agents=10, n_variables=2,
+                                      lower_bound=[0, 0], upper_bound=[10, 10])
+
+    new_pvs = pvs.PVS()
+
+    new_pvs.update(search_space, square)
