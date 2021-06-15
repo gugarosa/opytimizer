b941403a89952e6fbe7745e45fb2e6362cbb565a
Gustavo Rosa <gth.rosa@uol.com.br>
2021-06-15 14:40:42 -0300

    Although controversially, implementing the first draft of Sooty Tern Optimization Algorithm.

--- opytimizer/optimizers/swarm/stoa.py
+++ opytimizer/optimizers/swarm/stoa.py
@@ -41,7 +41,50 @@ class STOA(Optimizer):
         # Overrides its parent class with the receiving params
         super(STOA, self).__init__()
 
+        # Controlling variable
+        self.Cf = 2
+
+        #
+        self.u = 1
+
+        #
+        self.v = 1
+
         # Builds the class
         self.build(params)
 
         logger.info('Class overrided.')
+
+    def update(self, space, iteration, n_iterations):
+        """
+        """
+
+        # (eq. 2)
+        Sa = self.Cf - (iteration * (self.Cf / n_iterations))
+
+        # (eq. 4)
+        Cb = 0.5 * r.generate_uniform_random_number()
+
+        #
+        for agent in space.agents:
+            # (eq. 1)
+            C = Sa * agent.position
+
+            # (eq. 3)
+            M = Cb * (space.best_agent.position - agent.position)
+
+            # (eq. 5)
+            D = C + M
+
+            # (eq. 9)
+            k = r.generate_uniform_random_number(0, 2*np.pi)
+            R = self.u * np.exp(k * self.v)
+
+            # (eq. 6, 7 and 8)
+            i = r.generate_uniform_random_number(0, k)
+            x = R * np.sin(i)
+            y = R * np.cos(i)
+            z = R * i
+
+            # (eq. 10)
+            agent.position = (D * (x + y + z)) * space.best_agent.position
