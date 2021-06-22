5466e741545c97c8df1f5b014acc04b058784bed
Gustavo Rosa <gth.rosa@uol.com.br>
2021-06-22 16:21:49 -0300

    Removing LPOA-related files as it will not be implemented.

--- docs/api/opytimizer.optimizers.population.lpoa.rst
+++
@@ -1,7 +0,0 @@
-opytimizer.optimizers.population.lpoa
-======================================
-
-.. automodule:: opytimizer.optimizers.population.lpoa
-    :members:
-    :private-members:
-    :special-members:
--- examples/optimizers/population/create_lpoa.py
+++
@@ -1,9 +0,0 @@
-from opytimizer.optimizers.population import LPOA
-
-# One should declare a hyperparameters object based
-# on the desired algorithm that will be used
-params = {
-}
-
-# Creates an LPOA optimizer
-o = LPOA(params=params)
