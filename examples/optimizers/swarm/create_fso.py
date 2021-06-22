316506ea1d8f7e341d7efef76b1b5afc64613a79
Gustavo Rosa <gth.rosa@uol.com.br>
2021-06-22 16:01:53 -0300

    Implementing the first draft of Algorithm of the Innovative Gunner.

--- examples/optimizers/swarm/create_fso.py
+++ examples/optimizers/swarm/create_fso.py
@@ -3,7 +3,7 @@ from opytimizer.optimizers.swarm import FSO
 # One should declare a hyperparameters object based
 # on the desired algorithm that will be used
 params = {
-
+    'beta': 0.5
 }
 
 # Creates a FSO optimizer
--- opytimizer/optimizers/science/aig.py
+++ opytimizer/optimizers/science/aig.py
@@ -42,7 +42,47 @@ class AIG(Optimizer):
         # Overrides its parent class with the receiving params
         super(AIG, self).__init__()
 
+        # First correction angle
+        self.alpha = np.pi
+
+        # Second correction angle
+        self.beta = np.pi
+
         # Builds the class
         self.build(params)
 
         logger.info('Class overrided.')
+
+    def update(self, space, function):
+        # Calculating the maximum correction angles (eq. 18)
+        a = r.generate_uniform_random_number()
+        alpha_max = self.alpha * a
+        beta_max = self.beta * a
+
+        # Iterates through all agents
+        for agent in space.agents:
+            # Makes a deep copy of current agent
+            a = copy.deepcopy(agent)
+
+            # Sampling correction angles
+            alpha = r.generate_gaussian_random_number(0, alpha_max/3, (agent.n_variables, agent.n_dimensions))
+            beta = r.generate_gaussian_random_number(0, beta_max/3, (agent.n_variables, agent.n_dimensions))
+
+            # Calculating correction functions (eq. 16 and 17)
+            g_alpha = np.where(alpha < 0, np.cos(alpha), 1 / np.cos(alpha))
+            g_beta = np.where(beta < 0, np.cos(beta), 1 / np.cos(beta))
+
+            # Updating temporary agent's position
+            a.position *= g_alpha * g_beta
+
+            # Checks agent's limits
+            a.clip_by_bound()
+
+            # Re-evaluates the temporary agent
+            a.fit = function(a.position)
+
+            # If temporary agent's fitness is better than agent's fitness
+            if a.fit < agent.fit:
+                # Replace its position and fitness
+                agent.position = copy.deepcopy(a.position)
+                agent.fit = copy.deepcopy(a.fit)
