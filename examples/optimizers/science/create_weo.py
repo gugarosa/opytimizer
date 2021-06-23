a059d5ecc96512172e3c653c980d2436af27a6c5
Gustavo Rosa <gth.rosa@uol.com.br>
2021-06-23 11:34:49 -0300

    Finishing up WEO.

--- examples/optimizers/science/create_weo.py
+++ examples/optimizers/science/create_weo.py
@@ -1,9 +1,14 @@
+import numpy as np
+
 from opytimizer.optimizers.science import WEO
 
 # One should declare a hyperparameters object based
 # on the desired algorithm that will be used
 params = {
-
+    'E_min': -3.5,
+    'E_max': -0.5,
+    'theta_min': -np.pi / 3.6,
+    'theta_max': -np.pi / 9
 }
 
 # Creates a WEO optimizer
--- opytimizer/optimizers/science/weo.py
+++ opytimizer/optimizers/science/weo.py
@@ -4,12 +4,9 @@
 import copy
 
 import numpy as np
-from tqdm import tqdm
 
 import opytimizer.math.random as r
-import opytimizer.utils.constant as c
 import opytimizer.utils.exception as e
-import opytimizer.utils.history as h
 import opytimizer.utils.logging as l
 from opytimizer.core import Optimizer
 
@@ -59,16 +56,96 @@ class WEO(Optimizer):
 
         logger.info('Class overrided.')
 
-    def _evaporation_flux(self, theta):
-        """
+    @property
+    def E_min(self):
+        """float: Minimum substrate energy.
+
         """
 
-        # (eq. 7)
+        return self._E_min
+
+    @E_min.setter
+    def E_min(self, E_min):
+        if not isinstance(E_min, (float, int)):
+            raise e.TypeError('`E_min` should be a float or integer')
+
+        self._E_min = E_min
+
+    @property
+    def E_max(self):
+        """float: Maximum substrate energy.
+
+        """
+
+        return self._E_max
+
+    @E_max.setter
+    def E_max(self, E_max):
+        if not isinstance(E_max, (float, int)):
+            raise e.TypeError('`E_max` should be a float or integer')
+        if E_max < self.E_min:
+            raise e.ValueError('`E_max` should be >= `E_min`')
+
+        self._E_max = E_max
+
+    @property
+    def theta_min(self):
+        """float: Minimum contact angle.
+
+        """
+
+        return self._theta_min
+
+    @theta_min.setter
+    def theta_min(self, theta_min):
+        if not isinstance(theta_min, (float, int)):
+            raise e.TypeError('`theta_min` should be a float or integer')
+
+        self._theta_min = theta_min
+
+    @property
+    def theta_max(self):
+        """float: Maximum contact angle.
+
+        """
+
+        return self._theta_max
+
+    @theta_max.setter
+    def theta_max(self, theta_max):
+        if not isinstance(theta_max, (float, int)):
+            raise e.TypeError('`theta_max` should be a float or integer')
+        if theta_max < self.theta_min:
+            raise e.ValueError('`theta_max` should be >= `theta_min`')
+
+        self._theta_max = theta_max
+
+    def _evaporation_flux(self, theta):
+        """Calculates the evaporation flux (eq. 7).
+
+        Args:
+            theta (float): Radian-based angle.
+
+        Returns:
+            Evaporation flux.
+
+        """
+
+        # Calculates the evaporation flux (eq. 7)
         J = (1 / 2.6) * ((2 / 3 + np.cos(theta) ** 3 / 3 - np.cos(theta)) ** (-2 / 3)) * (1 - np.cos(theta))
 
         return J
 
-    def update(self, space, iteration, n_iterations):
+    def update(self, space, function, iteration, n_iterations):
+        """Wraps Water Evaporation Optimization over all agents and variables.
+
+        Args:
+            space (Space): Space containing agents and update-related information.
+            function (Function): A Function object that will be used as the objective function.
+            iteration (int): Current iteration.
+            n_iterations (int): Maximum number of iterations.
+
+        """
 
         # Sorts agents
         space.agents.sort(key=lambda x: x.fit)
@@ -78,39 +155,55 @@ class WEO(Optimizer):
 
         # Iterates through all agents
         for agent in space.agents:
+            # Makes a deep copy of current agent
+            a = copy.deepcopy(agent)
+
             # Checks whether it is the first half of iterations
             if int(iteration <= n_iterations / 2):
-                # (eq. 5)
-                E_sub = ((self.E_max - self.E_min) * (agent.fit - best.fit)) / (worst.fit - best.fit) + self.E_min
+                # Calculates the substrate energy (eq. 5)
+                E_sub = ((self.E_max - self.E_min) * (a.fit - best.fit)) / \
+                        (worst.fit - best.fit) + self.E_min
 
-                # (eq. 6)
+                # Calculates the Monolayer Evaporation Probability matrix (eq. 6)
                 r1 = r.generate_uniform_random_number(size=(agent.n_variables, agent.n_dimensions))
                 MEP = np.where(r1 < np.exp(E_sub), 1, 0)
 
-                # (eq. 10)
+                # Generates the step size (eq. 10)
                 r2 = r.generate_uniform_random_number()
                 i = r.generate_integer_random_number(0, space.n_agents)
                 j = r.generate_integer_random_number(0, space.n_agents, i)
                 S = r2 * (space.agents[i].position - space.agents[j].position)
 
-                # (eq. 11)
-                agent.position += S * MEP
+                # Updates the agent's position (eq. 11)
+                a.position += S * MEP
 
             # If it is the second half of iterations
             else:
-                pass
-                # (eq. 8)
-                theta = ((self.theta_max - self.theta_min) * (agent.fit - best.fit)) / (worst.fit - best.fit) + self.theta_min
+                # Calculates the contact angle (eq. 8)
+                theta = ((self.theta_max - self.theta_min) * (a.fit - best.fit)) / \
+                        (worst.fit - best.fit) + self.theta_min
 
-                # (eq. 9)
-                r1 = r.generate_uniform_random_number(size=(agent.n_variables, agent.n_dimensions))
+                # Calculates the Droplet Evaporation Probability matrix (eq. 9)
+                r1 = r.generate_uniform_random_number(size=(a.n_variables, a.n_dimensions))
                 DEP = np.where(r1 < self._evaporation_flux(theta), 1, 0)
 
-                # (eq. 10)
+                # Generates the step size (eq. 10)
                 r2 = r.generate_uniform_random_number()
                 i = r.generate_integer_random_number(0, space.n_agents)
                 j = r.generate_integer_random_number(0, space.n_agents, i)
                 S = r2 * (space.agents[i].position - space.agents[j].position)
 
-                # (eq. 11)
-                agent.position += S * DEP
+                # Updates the agent's position (eq. 11)
+                a.position += S * DEP
+
+            # Checks agent's limits
+            a.clip_by_bound()
+
+            # Re-evaluates the temporary agent
+            a.fit = function(a.position)
+
+            # If temporary agent's fitness is better than agent's fitness
+            if a.fit < agent.fit:
+                # Replace its position and fitness
+                agent.position = copy.deepcopy(a.position)
+                agent.fit = copy.deepcopy(a.fit)
