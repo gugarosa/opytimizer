e4258ed4f79094d0dc4a4d1161219720a907618f
douglasrodrigues <douglasrodrigues.dr@gmail.com>
2023-09-07 11:21:12 -0300

    Fix(opytimizer): Fixing update function signature

--- opytimizer/optimizers/swarm/waoa.py
+++ opytimizer/optimizers/swarm/waoa.py
@@ -9,6 +9,7 @@ import numpy as np
 import opytimizer.math.random as r
 import opytimizer.utils.logging as l
 from opytimizer.core.optimizer import Optimizer
+from opytimizer.core.function import Function
 from opytimizer.core.space import Space
 
 logger = l.get_logger(__name__)
@@ -40,23 +41,64 @@ class WAOA(Optimizer):
 
         logger.info('Class overrided.')
 
-    def update(self, space: Space) -> None:
+    def update(self, space: Space, function: Function, iteration: int) -> None:
         """Wraps Walrus Optimization Algorithm over all agents and variables.
 
         Args:
             space: Space containing agents and update-related information.
+            function: A Function object that will be used as the objective function.
+            iteration: Current iteration.
 
         """
 
-        for agent in space.agents:
+        for i, agent in enumerate(space.agents):
             a = copy.deepcopy(agent)
 
             for j in range(space.n_variables):
                 
-                k = r.generate_integer_random_number(1, 2)
-                r1 = r.generate_uniform_random_number()
+                r1 = r.generate_integer_random_number(1, 2)
+                r2 = r.generate_uniform_random_number()
 
-                a.position[j] = agent.position[j] + r1 * (space.best_agent.position[j] - k * agent.position[j])
+                a.position[j] = agent.position[j] + r2 * (space.best_agent.position[j] - r1 * agent.position[j])
+
+            a.clip_by_bound()
+
+            a.fit = function(a.position)
+            if a.fit < agent.fit:
+                agent.position = copy.deepcopy(a.position)
+                agent.fit = copy.deepcopy(a.fit)
+
+            k = r.generate_integer_random_number(0, space.n_agents, i)
+            
+            if (space.agents[k].fit < agent.fit):
+
+                for j in range(space.n_variables):
+                
+                    r1 = r.generate_integer_random_number(1, 2)
+                    r2 = r.generate_uniform_random_number()
+
+                    a.position[j] = agent.position[j] + r2 * (space.agents[k].position[j] - r1 * agent.position[j])
+            
+            else:
+
+                for j in range(space.n_variables):
+                
+                    r2 = r.generate_uniform_random_number()
+
+                    a.position[j] = agent.position[j] + r2 * (agent.position[j] - space.agents[k].position[j])
+            
+            a.clip_by_bound()
+
+            a.fit = function(a.position)
+            if a.fit < agent.fit:
+                agent.position = copy.deepcopy(a.position)
+                agent.fit = copy.deepcopy(a.fit)
+
+            for j in range(space.n_variables):
+
+                r2 = r.generate_uniform_random_number()
+
+                a.position[j] = agent.position[j] + ((agent.ub[j]/(iteration+1)) - r2 * (agent.lb[j]/(iteration+1)))
 
             a.clip_by_bound()
 
