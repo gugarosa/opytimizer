d058bc5f0ae9b32d7e072fda91815f26dbf976f4
douglasrodrigues <douglasrodrigues.dr@gmail.com>
2023-12-18 15:13:06 -0300

    fix(opytimizer): code optimization

--- opytimizer/optimizers/swarm/waoa.py
+++ opytimizer/optimizers/swarm/waoa.py
@@ -2,6 +2,7 @@
 """
 
 import copy
+import time
 from typing import Any, Dict, Optional
 
 import numpy as np
@@ -18,7 +19,7 @@ logger = l.get_logger(__name__)
 class WAOA(Optimizer):
     """A WAOA class, inherited from Optimizer.
 
-    This is the designed class to dife WAOA-related
+    This is the designed class to define WAOA-related
     variables and methods.
 
     References:
@@ -34,13 +35,27 @@ class WAOA(Optimizer):
             params (str): Contains key-value parameters to the meta-heuristics.
         """
 
-        logger.info("Overriding class: Optimizer -> SSA")
+        logger.info("Overriding class: Optimizer -> WAOA")
 
         super(WAOA, self).__init__()
 
         self.build(params)
 
         logger.info("Class overrided.")
+
+    def evaluate(self, space: Space) -> None:
+        """Evaluates the search space according to the objective function.
+
+        Args:
+            space: A Space object that will be evaluated.
+
+        """
+        print('evaluating...')
+        for agent in space.agents:
+            if agent.fit < space.best_agent.fit:
+                space.best_agent.position = copy.deepcopy(agent.position)
+                space.best_agent.fit = copy.deepcopy(agent.fit)
+                space.best_agent.ts = int(time.time())
 
     def update(self, space: Space, function: Function, iteration: int) -> None:
         """Wraps Walrus Optimization Algorithm over all agents and variables.
@@ -55,14 +70,16 @@ class WAOA(Optimizer):
         for i, agent in enumerate(space.agents):
             a = copy.deepcopy(agent)
 
-            for j in range(space.n_variables):
+            r1 = r.generate_integer_random_number(
+                1, 3, size=(space.n_variables, space.n_dimensions)
+            )
+            r2 = r.generate_uniform_random_number(
+                size=(space.n_variables, space.n_dimensions)
+            )
 
-                r1 = r.generate_integer_random_number(1, 2)
-                r2 = r.generate_uniform_random_number()
-
-                a.position[j] = agent.position[j] + r2 * (
-                    space.best_agent.position[j] - r1 * agent.position[j]
-                )
+            a.position = agent.position + r2 * (
+                space.best_agent.position - r1 * agent.position
+            )
 
             a.clip_by_bound()
 
@@ -75,24 +92,26 @@ class WAOA(Optimizer):
 
             if space.agents[k].fit < agent.fit:
 
-                for j in range(space.n_variables):
+                r3 = r.generate_integer_random_number(
+                    1, 3, size=(space.n_variables, space.n_dimensions)
+                )
+                r4 = r.generate_uniform_random_number(
+                    size=(space.n_variables, space.n_dimensions)
+                )
 
-                    r1 = r.generate_integer_random_number(1, 2)
-                    r2 = r.generate_uniform_random_number()
-
-                    a.position[j] = agent.position[j] + r2 * (
-                        space.agents[k].position[j] - r1 * agent.position[j]
-                    )
+                a.position = agent.position + r4 * (
+                    space.agents[k].position - r3 * agent.position
+                )
 
             else:
 
-                for j in range(space.n_variables):
+                r5 = r.generate_uniform_random_number(
+                    size=(space.n_variables, space.n_dimensions)
+                )
 
-                    r2 = r.generate_uniform_random_number()
-
-                    a.position[j] = agent.position[j] + r2 * (
-                        agent.position[j] - space.agents[k].position[j]
-                    )
+                a.position = agent.position + r5 * (
+                    agent.position - space.agents[k].position
+                )
 
             a.clip_by_bound()
 
@@ -101,14 +120,14 @@ class WAOA(Optimizer):
                 agent.position = copy.deepcopy(a.position)
                 agent.fit = copy.deepcopy(a.fit)
 
-            for j in range(space.n_variables):
+            r6 = r.generate_uniform_random_number(
+                size=(space.n_variables, space.n_dimensions)
+            )
 
-                r2 = r.generate_uniform_random_number()
+            lb = (agent.lb / (iteration + 1)).reshape(-1, 1)
+            ub = (agent.ub / (iteration + 1)).reshape(-1, 1)
 
-                a.position[j] = agent.position[j] + (
-                    (agent.ub[j] / (iteration + 1))
-                    - r2 * (agent.lb[j] / (iteration + 1))
-                )
+            a.position = agent.position + (lb + (ub - r6 * lb))
 
             a.clip_by_bound()
 
